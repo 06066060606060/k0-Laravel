f16347c713944795da59f5a68f26bc983ae42c0c
06066060606060 <xmicky+git@hotmail.fr>
2022-12-14 21:29:56 +0400

    fix barba swiper

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,9 +16,55 @@
         @include('parts.footer')
     </footer>
     @vite('resources/js/app.js')
+
     <script src="https://cdn.jsdelivr.net/npm/@barba/core"></script>
     <script>
        barba.init();
+        barba.hooks.afterEnter((data) => {
+        const swiper = new Swiper('.swiper-container', {
+            loop: true,
+            slidesPerView: 1.5,
+            spaceBetween: 24,
+            centeredSlides: true,
+            autoplay: {
+                delay: 8000,
+            },
+            breakpoints: {
+                640: {
+                    slidesPerView: 2,
+                },
+                800: {
+                    slidesPerView: 3,
+                },
+                1100: {
+                    slidesPerView: 4,
+                },
+            },
+        })
+});
+
+ document.addEventListener('DOMContentLoaded', function() {
+        const swiper = new Swiper('.swiper-container', {
+            loop: true,
+            slidesPerView: 1.5,
+            spaceBetween: 24,
+            centeredSlides: true,
+            autoplay: {
+                delay: 8000,
+            },
+            breakpoints: {
+                640: {
+                    slidesPerView: 2,
+                },
+                800: {
+                    slidesPerView: 3,
+                },
+                1100: {
+                    slidesPerView: 4,
+                },
+            },
+        })
+    })
     </script>
 </body>
 </html>
--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -447,27 +447,3 @@
         </div>
     </section>
 </container>
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        const swiper = new Swiper('.swiper-container', {
-            loop: true,
-            slidesPerView: 1.5,
-            spaceBetween: 24,
-            centeredSlides: true,
-            autoplay: {
-                delay: 8000,
-            },
-            breakpoints: {
-                640: {
-                    slidesPerView: 2,
-                },
-                800: {
-                    slidesPerView: 3,
-                },
-                1100: {
-                    slidesPerView: 4,
-                },
-            },
-        })
-    })
-</script>
