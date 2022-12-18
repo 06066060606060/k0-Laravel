ba11e50ea94f881a20e5ef07bad25797413142e6
06066060606060 <xmicky+git@hotmail.fr>
2022-12-18 21:50:43 +0400

    fix

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -18,19 +18,19 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
  * allows your team to easily build robust real-time web applications.
  */
 
-import Echo from 'laravel-echo';
+// import Echo from 'laravel-echo';
 
-import Pusher from 'pusher-js';
-window.Pusher = Pusher;
+// import Pusher from 'pusher-js';
+// window.Pusher = Pusher;
 
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_KEY,
-    wsHost: import.meta.env.VITE_PUSHER_HOST,
-    wsPort: import.meta.env.VITE_PUSHER_PORT,
-    forceTLS: false,
-    enabledTransports: ['ws'],
-});
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     wsHost: import.meta.env.VITE_PUSHER_HOST,
+//     wsPort: import.meta.env.VITE_PUSHER_PORT,
+//     forceTLS: false,
+//     enabledTransports: ['ws'],
+// });
 
 
 // key: import.meta.env.VITE_PUSHER_APP_KEY,
