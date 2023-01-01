be6088d564ab11c9df3c01e7e381738e0a7e754c
06066060606060 <xmicky+git@hotmail.fr>
2023-01-01 23:44:13 +0400

    up

--- app/Http/Middleware/XFrameOptions.php
+++ app/Http/Middleware/XFrameOptions.php
@@ -22,7 +22,7 @@ class XFrameOptions
 
         // In this example, we are only allowing the third party to include the "iframe" route
         // It's always better to scope this to a given route / set of routes to avoid any unattended security problems
-        if ($request->routeIs('iframe') && $xframeOptions = env('X_FRAME_OPTIONS', 'SAMEORIGIN')) {
+        if ($request->routeIs('game') && $xframeOptions = env('X_FRAME_OPTIONS', 'SAMEORIGIN')) {
             if (false !== strpos($xframeOptions, 'ALLOW-FROM')) {
                 $url = trim(str_replace('ALLOW-FROM', '', $xframeOptions));
 
