6fbbf1c98ea68c3a831fb9ccb97a873dde95b387
06066060606060 <xmicky+git@hotmail.fr>
2023-01-15 20:01:59 +0400

    up

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -17,7 +17,7 @@
                          <div class="container flex flex-col-reverse xl:flex-row">
                              <div class="w-full px-4 py-4 bg-gray-900 rounded-md xl:w-1/4 xl:mr-4">
                              <div id="points" class="py-2 text-sm font-normal text-gray-200"> &nbsp;</div>
-                                 <h1 class="font-extrabold text-md text-gray-50">My SCORES:</h1>
+                                 <h1 class="font-extrabold text-md text-gray-50">Mon SCORES:</h1>
                                  <table class="min-w-full divide-y divide-gray-200">
                                      <thead>
                                          <tr>
