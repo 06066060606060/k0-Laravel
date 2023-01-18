edde1d389666260ae5d5e071468feb025f1fde98
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-01-18 18:17:50 +0100

    ok

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -46,13 +46,13 @@
              <p class="text-sm text-center text-gray-400 sm:text-left">© 2023 GoKDO - Tous droits réservés
              </p>
              <span class="inline-flex justify-center mt-2 sm:ml-auto sm:mt-0 sm:justify-start">
-                 <a class="text-gray-400">
+                 <a href="https://www.facebook.com/gokdocom" class="text-gray-400">
                      <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                          class="w-5 h-5" viewBox="0 0 24 24">
                          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                      </svg>
                  </a>
-                 <a class="ml-3 text-gray-400">
+                 <a href="https://www.instagram.com/gokdocom/" class="ml-3 text-gray-400">
                      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
                          stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                          <rect width="20" height="20" x="2" y="2" rx="5"
