4435fa1d73429d53001929b4354d728e9ecf8aa6
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-01-18 17:28:06 +0100

    ok

--- resources/views/vendor/backpack/base/auth/passwords/email.blade.php
+++ resources/views/vendor/backpack/base/auth/passwords/email.blade.php
@@ -4,7 +4,6 @@
 @section('content')
     <div class="row justify-content-center">
         <div class="col-12 col-md-9 col-lg-6">
-            <h3 class="mb-4 text-center">{{ trans('backpack::base.reset_password') }}</h3>
              <a href="/">
                 <img class="w-auto pb-2 mx-auto h-1/6" src="/img/logo.png"></a>
             <div class="nav-steps-wrapper">
@@ -47,18 +46,19 @@
                         </div>
                     </form>
                     @endif
-                    <div class="clearfix"></div>
-                  </div>
-                  {{-- /.tab-pane --}}
-                </div>
-                {{-- /.tab-content --}}
-                   <div class="mt-4 text-center">
+                    <div class="mt-4 text-center">
                 <a href="{{ route('backpack.auth.login') }}">{{ trans('backpack::base.login') }}</a>
 
                 @if (config('backpack.base.registration_open'))
                 / <a href="{{ route('backpack.auth.register') }}">{{ trans('backpack::base.register') }}</a>
                 @endif
               </div>
+                    <div class="clearfix"></div>
+                  </div>
+                  {{-- /.tab-pane --}}
+                </div>
+                {{-- /.tab-content --}}
+                   
               </div>
 
            
