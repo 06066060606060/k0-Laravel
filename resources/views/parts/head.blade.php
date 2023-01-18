780687b521e87ef882eba4975cbc089ddfa3873f
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-01-18 23:46:06 +0100

    ok

--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -31,3 +31,15 @@
 <script>
     myToken = <?php echo json_encode(['csrfToken' => csrf_token()]); ?>
 </script>
+
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/63c87381c2f1ac1e202e6cae/1gn3g6d7p';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
