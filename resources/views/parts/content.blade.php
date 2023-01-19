6bb813141b60ca80ce05bce9441a9f5b234b4896
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-01-19 18:04:24 +0100

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -1,6 +1,6 @@
 @php use \App\Http\Controllers\GlobalController;
   $check_mobile = GlobalController::mafonction();
-if(check_mobile())
+if($is_mobile == false)
 else
 {{ '<div class="z-0 one"></div>' }}
 endif
