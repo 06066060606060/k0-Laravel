2f2e02a191adef2054522bfa32bc29eb49136e3a
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-01-19 19:43:26 +0100

    ok

--- resources/views/vendor/backpack/base/inc/head.blade.php
+++ resources/views/vendor/backpack/base/inc/head.blade.php
@@ -62,6 +62,6 @@
             // Suppression de la notification après 5 secondes
             setTimeout(() => {
                 $(notification).alert('close');
-            }, 5000);
+            }, 10000);
         }
     </script>
