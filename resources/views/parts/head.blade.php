e29a065aabf46bc861d37c9b8d62cc7f9da4024d
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-01-19 18:47:52 +0100

    ok

--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -51,24 +51,26 @@ document.tidioIdentify = {
 };
 </script>
 @endif
-
-<script>
-function createNotification(message) {
-    // Création d'un élément de notification
-    let notification = document.createElement("div");
-    notification.classList.add("alert", "alert-info", "fade", "show");
-    notification.innerHTML = message;
-    notification.setAttribute("role", "alert");
-    // Ajout de la notification à la page
-    document.getElementById("notification-container").appendChild(notification);
-
-    // Suppression de la notification après 5 secondes
-    setTimeout(() => {
-        $(notification).alert('close');
-    }, 5000);
-}
-</script>
-<div id="notification-container"></div>
-<script>
-createNotification("Votre message ici");
-</script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div id="notification-container"></div>
+    <script>
+        function createNotification(message) {
+            // Création d'un élément de notification
+            let notification = document.createElement("div");
+            notification.classList.add("alert", "alert-info", "fade", "show");
+            notification.innerHTML = message;
+            notification.setAttribute("role", "alert");
+            // Ajout de la notification à la page
+            document.getElementById("notification-container").appendChild(notification);
+            // Suppression de la notification après 5 secondes
+            setTimeout(() => {
+                $(notification).alert('close');
+            }, 5000);
+        }
+        createNotification("Votre message ici");
+    </script>
