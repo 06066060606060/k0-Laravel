9d60e4970d4ea2125bb36bac74c9fc0e0e06536e
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-01-19 18:29:21 +0100

    ok

--- resources/views/pack.blade.php
+++ resources/views/pack.blade.php
@@ -6,7 +6,14 @@
         data-sdk-integration-source="button-factory"></script>
 
     <div data-barba="container">
+    @php
+        use \App\Http\Controllers\GlobalController;
+        $isMobile = GlobalController::isMobile();
+    @endphp
+    @if($isMobile == true)
+    @else
         <div class="z-0 one"></div>
+    @endif
         <container class="block px-4 mx-auto text-white max-w-7xl">
             <div class="container px-5 pt-8 mx-auto">
                 <div class="flex flex-col w-full mb-10 text-center">
