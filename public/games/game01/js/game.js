e0e349e35f563057a8e08ebffd8af83aa7923689
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-01-22 17:49:10 +0100

    ok

--- public/games/game01/js/game.js
+++ public/games/game01/js/game.js
@@ -46,7 +46,7 @@ var plinkoSettings = {
 		{ value: 0, text: "0", fontSize: 20, lineHeight: 18, x: 0, y: 5, color: "#fff", bgColor: "#7700B0", bgWinColor: "#A600F9", percent: 0 },
 		{ value: 500, text: "5\n0\n0", fontSize: 20, lineHeight: 18, x: 0, y: -8, color: "#fff", bgColor: "#7700B0", bgWinColor: "#A600F9", percent: 0 },
 		{ value: 0, text: "0", fontSize: 20, lineHeight: 18, x: 0, y: 5, color: "#fff", bgColor: "#7700B0", bgWinColor: "#A600F9", percent: 0 },
-		{ value: 250, text: "2\n5\n0", fontSize: 20, lineHeight: 18, x: 0, y: -8, color: "#fff", bgColor: "#7700B0", bgWinColor: "#A600F9", percent: 00 },
+		{ value: 250, text: "2\n5\n0", fontSize: 20, lineHeight: 18, x: 0, y: -8, color: "#fff", bgColor: "#7700B0", bgWinColor: "#A600F9", percent: 0 },
 		{ value: 0, image: 'assets/item_prize_bonus.png', x: 0, y: 0, color: "#fff", bgColor: "#7700B0", bgWinColor: "#A600F9", percent: 0, bonus: true },
 		{ value: 50, text: "5\n0", fontSize: 20, lineHeight: 18, x: 0, y: -3, color: "#fff", bgColor: "#7700B0", bgWinColor: "#A600F9", percent: 0 },
 	]
