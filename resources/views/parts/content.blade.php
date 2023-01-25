4bd6c2843e87f0521855e06dc8bd05e368428df9
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-01-25 18:20:50 +0100

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -276,10 +276,10 @@
                             <div class="absolute top-0 right-0 w-16 h-16">
                                 <div
                                     class="border z-20 absolute transform rotate-45 select-none bg-orange-800 text-center text-white font-semibold py-1 right-[-50px] top-[20px] w-[170px] shadow-lg">
-                                {{ $boostergames->prix }}
-                                @if ($boostergames->type_prix == 'Diamants')
+                                {{ $boostergame->prix }}
+                                @if ($boostergame->type_prix == 'Diamants')
                                  <img src="img/diamond5.png" class="w-4" style="display:inline;">
-                                @elseif ($boostergames->type_prix == 'Rubis')
+                                @elseif ($boostergame->type_prix == 'Rubis')
                                  <img src="img/gem10.png" class="w-4" style="display:inline;">
                                 @else
                                  <img src="img/coin10.png" class="w-4" style="display:inline;">
