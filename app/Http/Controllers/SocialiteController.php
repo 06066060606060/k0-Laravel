d43acb4cdbf56fd8bbb91b5cd7d16f1d55e20758
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-01-26 14:55:44 +0100

    ok

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -68,7 +68,7 @@ class SocialiteController extends Controller
                     'trophee1' => '150' // On offre 150 diamants
                 ]);
                 //create notification
-                $admin = backpack_auth()->find(1);
+                
                 $admin->notify(
                     new DatabaseNotification(
                         ($type = 'info'), // info / success / warning / error
