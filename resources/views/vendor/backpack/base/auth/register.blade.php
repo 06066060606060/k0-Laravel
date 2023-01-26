9fd4d2636b228da54ef8a7f48a66fcf834cc23e5
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-01-26 17:25:12 +0100

    ok

--- resources/views/vendor/backpack/base/auth/register.blade.php
+++ resources/views/vendor/backpack/base/auth/register.blade.php
@@ -124,7 +124,7 @@
             @endif
             <div class="text-center"><a href="{{ route('backpack.auth.login') }}">{{ trans('backpack::base.login') }}</a> /
             <a href="{{ route('backpack.auth.password.reset') }}">{{ trans('backpack::base.forgot_your_password') }}</a></div>
-               <div class="text-center"><br>En vous iscrivant vous acceptez le <a href="/rules">règlement</a></div>
+               <div class="text-center"><br>En vous iscrivant vous acceptez le <a href="/rules">règlement</a></div></div>
             </div>
           
         </div>
