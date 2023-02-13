5622eeedc9a6948463df8242d23a29adfa5643c0
06066060606060 <xmicky+git@hotmail.fr>
2023-02-13 19:06:52 +0400

    fix

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -17,7 +17,7 @@
 <li class="nav-item nav-dropdown">
     <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon las la-trophy"></i> Concours</a>
     <ul class="nav-dropdown-items">
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('concours') }}"><i class="nav-icon la-trophy "></i> Concours</a>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('concours') }}"><i class="nav-icon las la-trophy "></i> Concours</a>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('gain') }}"><i class="nav-icon la la-th-list"></i> Gains</a></li>
     </ul>
 </li>
