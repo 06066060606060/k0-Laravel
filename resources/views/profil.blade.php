5f660eb9c5b3360ef126a58a85c3d5a6a3bc4f40
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-02 18:26:26 +0200

    ok

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -219,6 +219,7 @@
                                         <td class="px-4 py-2 text-gray-300 whitespace-nowrap"> {{ $score->score }}</td>
                                         <td class="justify-center hidden w-auto py-2 mx-auto md:flex whitespace-nowrap">
                                             <strong class="flex md:px-3 py-1.5 text-xs font-bold  text-white max-w-[180px]">
+                                                <p class="ml-2 ">{{ $score->created_at->format('d/m H:i') }}</p> 
                                             </strong>
 
                                         </td>
