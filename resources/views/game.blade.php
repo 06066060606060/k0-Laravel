8ff512d4a24ea3dbe702594771bbfabc3264fdf2
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-02 11:00:14 +0200

    ok

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -44,8 +44,8 @@
                                                  <td class="px-4 py-2 text-sm font-normal text-gray-200 whitespace-nowrap">
                                                      {{ $score->user->name }}
                                                  </td>
-                                                 <td class="px-4 py-2 text-sm font-normal text-gray-200 rate-container">
-                                                     {{ $score->data }} <img src="img/diamond5.png" class="mt-1 ml-2 w-6 h-4">
+                                                 <td class="px-4 py-2 text-sm font-normal text-gray-200 rate-container" style="display:inline;">
+                                                     {{ $score->data }} <img src="img/diamond5.png" class="mt-1 ml-2 w-6 h-4" style="display:inline;">
 
                                                  </td>
 
