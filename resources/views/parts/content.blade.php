ac4aee9f16b3832cb2a353573d36f9cd043528b9
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-07 15:54:17 +0200

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -162,11 +162,11 @@
 
             <div class="flex flex-col w-full mb-20 text-center">
                 <h1 class="mb-4 text-4xl font-bold text-gray-100 md:text-5xl title-font">Jouez !</h1>
-                <p class="mx-auto text-base leading-relaxed text-gray-300 lg:w-2/3"><a class="text-blue-500 prevent">Deux jeux</a> sont proposés hors évènements ! <br>
-                - Un jeu totalement gratuit avec 10 parties offertes toutes les 24h.
+                <p class="mx-auto text-base leading-relaxed text-gray-300 lg:w-2/3">Nous vous proposons deux <a class="text-blue-500 prevent">
+                 jeux multijoueurs en temps réel</a>, hors évènements et rush ! <br>
+                - Un <a class="text-blue-500 prevent">jeu totalement gratuit</a> avec 10 parties offertes toutes les 24h.
                 <br>
-                - Un jeu 100% gagnant pour lequel 10 Rubis sont requis !<br>
-                Ces jeux sont multijoueurs en temps réel.
+                - Un <a class="text-blue-500 prevent">jeu 100% gagnant</a> pour lequel 10 Rubis sont requis !
                 </p>
             </div>
             <div class="flex flex-wrap -m-4">
