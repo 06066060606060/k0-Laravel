2b7e6504ed94fd0d187801212fc2486095bb2fa3
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-07 22:45:08 +0200

    ok

--- resources/views/legal.blade.php
+++ resources/views/legal.blade.php
@@ -12,7 +12,7 @@
         <div class="z-0 one"></div>
     @endif
                  <div class="text-gray-100 mb-4 px-2 py-4 mx-8 bg-gray-800 rounded-lg lg:mx-8 xl:mx-auto bg-opacity-40 max-w-7xl sm:px-16 md:px-24 lg:py-18">
-            <div class="flex flex-col max-w-5xl mx-auto overflow-hidden rounded">
+            <div class="flex flex-wrap -m-4">
 
                 <div class="text-white reverted">
                     @php
