18135d058a020dc05b887fee5f890dc84d95d067
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-08 10:43:41 +0200

    ok

--- resources/views/aide.blade.php
+++ resources/views/aide.blade.php
@@ -12,36 +12,39 @@
                         <h1 class="mb-4 text-4xl font-bold text-gray-100 md:text-5xl title-font">Espace Aide</h1>
                     </div>
                     <div class="mx-auto lg:w-1/2 md:w-2/3">
-                        <div class="container">
-  <ul class="nav nav-tabs" id="myTab" role="tablist">
-    <li class="nav-item">
-      <a class="nav-link active" id="tab1-tab" data-toggle="tab" href="#tab1" role="tab" aria-controls="tab1" aria-selected="true">Comment jouer au jeu GoFRUITS ?</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" id="tab2-tab" data-toggle="tab" href="#tab2" role="tab" aria-controls="tab2" aria-selected="false">Comment jouer au jeu Pool ?</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" id="tab3-tab" data-toggle="tab" href="#tab3" role="tab" aria-controls="tab3" aria-selected="false">Comment acheter des Rubis ?</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" id="tab4-tab" data-toggle="tab" href="#tab4" role="tab" aria-controls="tab4" aria-selected="false">Comment choisir un cadeau ?</a>
-    </li>
-  </ul>
-  <div class="tab-content" id="myTabContent">
-    <div class="tab-pane fade show active" id="tab1" role="tabpanel" aria-labelledby="tab1-tab">
-      <center><iframe width="100%" height="400" src="https://www.youtube.com/embed/3-BmXAOkgvI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></center>
-    </div>
-    <div class="tab-pane fade" id="tab2" role="tabpanel" aria-labelledby="tab2-tab">
-      <p>Contenu 2</p>
-    </div>
-    <div class="tab-pane fade" id="tab3" role="tabpanel" aria-labelledby="tab3-tab">
-      <p>Contenu 3</p>
-    </div>
-    <div class="tab-pane fade" id="tab4" role="tabpanel" aria-labelledby="tab4-tab">
-      <p>Contenu 4</p>
-    </div>
-  </div>
-</div>
+                        <div class="tabs">
+                            <ul class="tab-titles text-white">
+                                <li class="active">
+                                    <h2><a href="#tab1">Comment jouer au jeu GoFRUITS ?</a></h2>
+                                </li>
+                                <li>
+                                    <h2><a href="#tab2">Comment jouer au jeu Pool ?</a></h2>
+                                </li>
+                                <li>
+                                    <h2><a href="#tab3">Comment acheter des Rubis ?</a></h2>
+                                </li>
+                                <li>
+                                    <h2><a href="#tab4">Comment choisir un cadeau ?</a></h2>
+                                </li>
+                            </ul>
+                            <div class="tab-content mt-4">
+                                <div class="tab-pane active" id="tab1">
+                                    <p>
+                                        <center><iframe width="100%" height="400" src="https://www.youtube.com/embed/3-BmXAOkgvI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                        </center>
+                                    </p>
+                                </div>
+                                <div class="tab-pane" id="tab2">
+                                    <p>Contenu 2</p>
+                                </div>
+                                <div class="tab-pane" id="tab3">
+                                    <p>Contenu 3</p>
+                                </div>
+                                <div class="tab-pane" id="tab4">
+                                    <p>Contenu 4</p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </section>
