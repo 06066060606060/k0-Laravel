b55d168a0242522f61815cd37277d42c8cf0dcea
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-08 17:06:59 +0200

    ok

--- app/Session/DatabaseSessionHandler.php
+++ app/Session/DatabaseSessionHandler.php
@@ -11,21 +11,29 @@ class DatabaseSessionHandler extends \Illuminate\Session\DatabaseSessionHandler
      */
     public function write($sessionId, $data): bool
     {
-       
+        // On récupère l'ID de l'utilisateur connecté ou null s'il n'y en a pas.
         $user_id = (backpack_auth()->check()) ? backpack_auth()->user()->id : null;
       
-        if ($this->exists) {
+        if ($this->exists()) {
+            // Si la session existe, on met à jour les informations de celle-ci.
             $this->getQuery()->where('id', $sessionId)->update([
-                'payload' => base64_encode($data), 'last_activity' => time(), 'user_id' => $user_id,
+                'payload' => base64_encode($data),
+                'last_activity' => time(),
+                'user_id' => $user_id,
             ]);
         } else {
+            // Sinon, on crée une nouvelle session.
             $this->getQuery()->insert([
-                'id' => $sessionId, 'ip_address' => 'ipaddress', 'user_agent' => 'useragent', 'payload' => base64_encode($data), 'last_activity' => time(), 'user_id' => $user_id,
+                'id' => $sessionId,
+                'ip_address' => 'adresseIP',
+                'user_agent' => 'agentUtilisateur',
+                'payload' => base64_encode($data),
+                'last_activity' => time(),
+                'user_id' => $user_id,
             ]);
         }
-
-       return $this->exists = true;
-        
+    
+        // On retourne true pour indiquer que la session existe.
+        return $this->exists() = true;
     }
-
 }
