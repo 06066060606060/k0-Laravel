2362e12c9a6dd583a1587da3c7239cb69a7c4887
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-10 01:56:59 +0200

    ok

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,11 +20,11 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role')->default('user');
-            $table->integer('trophee1')->default(150)->nullable();
+            $table->integer('trophee1')->default(0)->nullable();
             $table->integer('trophee2')->default(0)->nullable();
             $table->decimal('trophee3', 4, 2)->default(0)->nullable();
             $table->integer('global_score')->default(0)->nullable();
-            $table->integer('parties')->default(10)->nullable();
+            $table->integer('parties')->default(0)->nullable();
             $table->integer('concours_parties')->default(0)->nullable();
             $table->integer('concours_score')->default(0)->nullable();
             $table->rememberToken();
