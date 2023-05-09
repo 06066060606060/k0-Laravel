aa08f749eeb5896f7729354a4df3bacb49a4f6d5
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-09 20:24:11 +0200

    ok

--- app/Console/Commands/UpdateUsers.php
+++ app/Console/Commands/UpdateUsers.php
@@ -8,7 +8,7 @@ use Illuminate\Foundation\Auth\User as AuthUser;
 use Illuminate\Support\Facades\DB;
 use Laravel\Socialite\One\User as OneUser;
 
-class UpdateUsersParties extends Command
+class UpdateUsers extends Command
 {
     /**
      * The name and signature of the console command.
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     protected $commands = [
-        \App\Console\Commands\UpdateUsersParties::class,
+        \App\Console\Commands\UpdateUsers::class,
     ];
 
     /**
