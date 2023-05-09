7fec053902d4e95880c2e7167957fed850a96c0b
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-09 21:27:41 +0200

    ok

--- app/Console/Commands/UpdateUsers.php
+++ app/Console/Commands/UpdateUsers.php
@@ -5,7 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use App\Models\User;
 
-class UsersUpdate extends Command
+class UpdateUsers extends Command
 {
     /**
      * The name and signature of the console command.
