13a3af3c0c9975ce1f9edb5f14848e434d406897
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-09 23:30:19 +0200

    ok

--- app/Console/Commands/UpdateUsers.php
+++ app/Console/Commands/UpdateUsers.php
@@ -33,7 +33,7 @@ class UpdateUsers extends Command
      */
     public function handle()
 {
-    User::where('uid', '!=', '1')->update([
+    where('id', '!=', '1')->update([
         'parties' => 10,
     ]);         
     $this->info('Users parties updated successfully!');
