6b4c909bee02995347331b2e39274f845ed187fb
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-09 18:28:14 +0200

    ok

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('update:users')->daily();
+        $schedule->command('update:users');
     }
 
     /**
