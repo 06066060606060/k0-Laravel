1df7d49c045dadf0f71fc70bd99e5b72283fe2e0
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-09 16:20:07 +0200

    ok

---
+++ app/Console/Commands/UpdateUsersParties.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class UpdateUsersParties extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+{
+    DB::table('users')->update(['parties' => 10]);
+    $this->info('Users parties updated successfully!');
+}
+
+}
