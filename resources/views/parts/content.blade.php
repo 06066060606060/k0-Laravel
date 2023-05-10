184920c8d05ae6d36eaa74525fa76fc54bcadc44
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-10 23:16:57 +0200

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -157,7 +157,7 @@
                                     @elseif($score->data3 > 0)
                                               <span href="#" class="ml-4 text-m font-bold text-blue-700 lg:mb-0">{{ $score->data3 }}</span>
                                     @endif
-                                    <span href="#" class="ml-4 text-s font-bold text-green-700 lg:mb-0">Sur le jeu Pool</span>
+                                    <span href="#" class="ml-4 text-s font-bold text-green-800 lg:mb-0">Sur le jeu Pool</span>
                                     
 
                                         </div>
