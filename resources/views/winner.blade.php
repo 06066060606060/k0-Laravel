699dcc51ba86298a4ba607258755f26e2468059d
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-10 08:54:52 +0200

    ok

--- resources/views/winner.blade.php
+++ resources/views/winner.blade.php
@@ -118,6 +118,11 @@
                                                       {{ $score->user->name }}
                                                  </td>
                                                  <td class="px-4 pt-2 text-left text-gray-200 whitespace-nowrap">
+                                                    <strong class="flex rounded md:px-3 py-1.5 text-xs font-bold  bg-red-600 text-white max-w-[180px]">
+                                                        <p class="hidden ml-1 md:block">+ {{ $score->data }}</p>  <img src="{{ asset('img/diamond5.png') }}" alt="coin" class="w-4 h-4 ml-2"> 
+                                                        <p class="hidden ml-1 md:block">+ {{ $score->data2 }}</p>  <img src="{{ asset('img/coin10.png') }}" alt="coin" class="w-4 h-4 ml-2">
+                                                         <p class="hidden ml-1 md:block">+ {{ $score->data3 }}</p>  <img src="{{ asset('img/gem5.png') }}" alt="coin" class="w-4 h-4 ml-2">
+                                                     </strong>
                                                     {{ $score->score }}
                                                  </td>
                                                  <td class="whitespace-nowrap px-4 py-2 w-[250px]">
