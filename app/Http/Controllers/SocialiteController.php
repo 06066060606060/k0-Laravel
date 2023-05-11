dae4450cf3e548a0c65dd054f2070e5eead31abd
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-11 14:22:52 +0200

    ok

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -33,61 +33,58 @@ class SocialiteController extends Controller
         abort(404); // Si le provider n'est pas autorisé
     }
 
-    // Callback du provider
-    public function callback (Request $request) {
+// Callback du provider
+public function callback (Request $request) {
 
-        $provider = $request->provider;
+    $provider = $request->provider;
 
-        if (in_array($provider, $this->providers)) {
+    if (in_array($provider, $this->providers)) {
 
-        	// Les informations provenant du provider
-            $data = Socialite::driver($provider)->stateless()->user();
-          
-            # Social login - register
-            $email = $data->getEmail(); // L'adresse email
-            $name = $data->getName(); // le nom
-           
-            # 1. On récupère l'utilisateur à partir de l'adresse email
-            $user = User::where("email", $email)->first();
-            # 2. Si l'utilisateur existe
-            if (isset($user)) {
+        // Les informations provenant du provider
+        $data = Socialite::driver($provider)->stateless()->user();
+      
+        # Social login - register
+        $email = $data->getEmail(); // L'adresse email
+        $name = $data->getName(); // le nom
+       
+        # 1. On récupère l'utilisateur à partir de l'adresse email
+        $user = User::where("email", $email)->first();
+        # 2. Si l'utilisateur existe
+        if (isset($user)) {
 
-                // Mise à jour des informations de l'utilisateur
-                $user->name = $name;
-                $user->save();
+            // Mise à jour des informations de l'utilisateur
+            $user->name = $name;
+            $user->save();
 
-            # 3. Si l'utilisateur n'existe pas, on l'enregistre
-            } else {
-                
-                // Enregistrement de l'utilisateur
-                $user = User::create([
-                    'name' => $name,
-                    'email' => $email,
-                    'role' => 'user',
-                    'password' => bcrypt("emiliedghioljfydesretyuioiuytrds"), // On fait un mot de passe
-                    'parties' => '10', // on ajoute 10 parties gratuites
-                    'trophee1' => '150' // On offre 150 diamants
-                ]);
-                //create notification
-                $admin = User::where('role', 'admin')->first();
-                $admin->notify(
-                    new DatabaseNotification(
-                        ($type = 'info'), // info / success / warning / error
-                        ($message = 'Nouvelle Inscription'),
-                        ($messageLong = 'Nouvelle Inscription: ' . $email)
-                    )
-                );
-                
-            }
+        # 3. Si l'utilisateur n'existe pas, on l'enregistre
+        } else {
+            
+            // Enregistrement de l'utilisateur
+            $user = User::create([
+                'name' => $name,
+                'email' => $email,
+                'role' => 'user',
+                'password' => bcrypt("emiliedghioljfydesretyuioiuytrds"), // On fait un mot de passe
+                'parties' => '10', // on ajoute 10 parties gratuites
+                'trophee1' => '150' // On offre 150 diamants
+            ]);
+            //create notification
+            $admin = User::where('role', 'admin')->first();
+            $admin->notify(
+                new DatabaseNotification(
+                    ($type = 'info'), // info / success / warning / error
+                    ($message = 'Nouvelle Inscription'),
+                    ($messageLong = 'Nouvelle Inscription: ' . $email)
+                )
+            );
+            
+        }
 
-            # 4. On connecte l'utilisateur
-            backpack_auth()->login($user);
+        # 4. On connecte l'utilisateur
+        backpack_auth()->login($user);
 
-            # 5. On redirige l'utilisateur vers /home
-            if (backpack_auth()->check()) return redirect('/');
-
-         }
-         abort(404);
-    }
-
+        # 5. On redirige l'utilisateur vers /home avec un message de succès
+        return redirect('/')->with('success', 'Vous êtes maintenant connecté avec ' . ucfirst($provider) . '.');
+     }
+     abort(404);
 }
