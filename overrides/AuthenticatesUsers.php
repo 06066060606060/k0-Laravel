4e6849208d21621f80910a9a26f0d418f6ca1ad6
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-12 14:04:39 +0200

    ok

--- overrides/AuthenticatesUsers.php
+++ overrides/AuthenticatesUsers.php
@@ -163,20 +163,21 @@ trait AuthenticatesUsers
      * @return \Illuminate\Http\Response
      */
     public function logout(Request $request)
-{
-    $this->guard()->logout();
+    {
+        $this->guard()->logout();
 
-    $request->session()->invalidate();
+        $request->session()->invalidate();
 
-    $request->session()->regenerateToken();
+        $request->session()->regenerateToken();
 
-    if ($response = $this->loggedOut($request)) {
-        return $response;
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new Response('', 204)
+            : redirect('/');
     }
-
-    return redirect('https://gokdo.com/');
-}
-
 
     /**
      * The user has logged out of the application.
