cb1ae1c537e5a9200c6ef8cce3cffb8cfcf81ee5
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-12 19:46:54 +0200

    ok

--- routes/backpack/custom.php
+++ routes/backpack/custom.php
@@ -10,10 +10,10 @@ use App\Http\Middleware\MyMiddleware;
 // Routes you generate using Backpack\Generators will be placed here.
 
 Route::group([
-    'prefix'     => config('backpack.base.route_prefix', ''),
+    'prefix'     => config('backpack.base.route_prefix', 'admin'),
     'middleware' => array_merge(
         (array) config('backpack.base.web_middleware', 'web'),
-        (array) config('backpack.base.middleware_key', 'admin')
+        (array) config('backpack.base.middleware_key', '')
     ),
     'namespace'  => 'App\Http\Controllers\Admin',
 ], function () { // custom admin routes
