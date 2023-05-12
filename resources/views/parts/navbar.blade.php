6a901532135fbb007540d812e5e8b99cafa4d193
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-12 12:27:30 +0200

    ok

--- resources/views/parts/navbar.blade.php
+++ resources/views/parts/navbar.blade.php
@@ -100,7 +100,7 @@
                                             Administration</a>
                                     @endif
                                     <a href="admin/logout"
-                                        class="block px-4 py-3 text-sm font-bold text-gray-300 capitalize transition-colors duration-300 transform hover:bg-gray-700 hover:text-white prevent">
+                                        class="block px-4 py-3 text-sm font-bold text-gray-300 capitalize transition-colors duration-300 transform hover:bg-gray-700 hover:text-white prevent" data-barba-prevent="self">
                                         Déconnexion </a>
                                 </div>
 
