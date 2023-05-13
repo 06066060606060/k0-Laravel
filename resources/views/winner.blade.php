e48874bc1322337e0a498fcfc62cb3026cec0cf3
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-13 12:25:11 +0200

    ok

--- resources/views/winner.blade.php
+++ resources/views/winner.blade.php
@@ -216,7 +216,7 @@
             $('#exampleModal').modal('show');
         });
     </script>     
-<img src="{{ asset('img/trophy.png') }}"/>
+<center><img src="{{ asset('img/trophy.png') }}"/></center>
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
