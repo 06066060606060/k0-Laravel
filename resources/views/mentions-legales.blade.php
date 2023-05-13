d9f387c5f42468363587a159e481c4fd4de10b3e
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-13 10:38:27 +0200

    ok

--- resources/views/mentions-legales.blade.php
+++ resources/views/mentions-legales.blade.php
@@ -5,7 +5,7 @@
 <div data-barba="container">
     @php use \App\Http\Controllers\GlobalController; @endphp
     <div class="mb-4 px-2 py-4 mx-8 bg-gray-800 rounded-lg lg:mx-8 xl:mx-auto bg-opacity-40 max-w-7xl sm:px-16 md:px-24 lg:py-18">
-        <div class="flex flex-col max-w-5xl pb-8 mx-auto mb-8 overflow-hidden rounded">
+        <div class="flex flex-col max-w-5xl pb-0 mx-auto mb-2 overflow-hidden rounded">
             <section class="relative text-gray-600 body-font">
                 <div class="container px-5 py-8 mx-auto">
                     <div class="flex flex-col w-full mb-4 text-center">
