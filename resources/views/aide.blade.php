dd23461e53c6f295dc9483e89f77fa86c2ea6f62
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-20 20:34:56 +0200

    ok

--- resources/views/aide.blade.php
+++ resources/views/aide.blade.php
@@ -12,8 +12,8 @@
                         <h1 class="mb-4 text-4xl font-bold text-gray-100 md:text-5xl title-font">Espace Aide</h1>
                     </div>
                     <div class="mx-auto lg:w-1/1 md:w-1/1">
-                    <h2 class="mb-1 text-2xl font-bold text-blue-500 title-font">Site et Connexion</h2>
-                    <div class="accordion-section bg-gray-800 bg-opacity-40 mb-4">
+                    <h2 class="mb-2 text-2xl font-bold text-blue-500 title-font">Site et Connexion</h2>
+                    <div class="accordion-section bg-gray-800 bg-opacity-40 mb-6">
                     <h2 class="accordion-title text-l">L'accès au site est-il gratuit ?</h2>
                     <div class="accordion-content mb-4">
                         <div class="px-6 py-6 text-white">L'accès au site est entièrement gratuit. Vous n'avez à payer que votre connexion à Internet ; il n'y a aucun frais supplémentaire. Gokdo est un site de jeux gratuits sans obligation d'achat !</div>
