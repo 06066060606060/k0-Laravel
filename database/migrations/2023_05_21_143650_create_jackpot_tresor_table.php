89d24c0bf359accaabee3732263f4c20391d0252
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-21 16:38:15 +0200

    ok

--- database/migrations/2023_05_21_143650_create_jackpot_tresor_table.php
+++ database/migrations/2023_05_21_143650_create_jackpot_tresor_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('jackpot_tresor', function (Blueprint $table) {
             $table->id();
+            $table->integer('jackpot');
+            $table->string('dernier_gagnant');
+            $table->timestamp('date_gagnant')->nullable();
             $table->timestamps();
         });
     }
