c0161168865c3cedb7f4ba4fd517cc6e728900b5
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-26 01:48:38 +0200

    ok

---
+++ lang/en/footer.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'f1' => 'Multiplayer Online Games',
+    'f2' => 'Win gifts by playing!',
+    'f3' => "GoKDO is a free gaming website with no purchase obligation.",
+    'f4' => 'About',
+    'f5' => 'Rules',
+    'f6' => 'Legal Notices',
+    'f7' => 'Privacy',
+    'f8' => 'More',
+    'f9' => 'Help',
+    'f10' => 'Contact Us',
+    'f11' => 'All rights reserved',
+
+];
---
+++ lang/fr/footer.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'f1' => 'Jeux en ligne Multijoueurs',
+    'f2' => 'Gagnez des cadeaux en jouant !',
+    'f3' => "GoKDO est un site de jeux gratuits sans obligation d'achat.",
+    'f4' => 'A Propos',
+    'f5' => 'Règlement',
+    'f6' => "Mentions Légales",
+    'f7' => 'Confidentialité',
+    'f8' => "Plus",
+    'f9' => 'Aide',
+    'f10' => "Nous Contacter",
+    'f11' => 'Tous droits réservés',
+
+];
