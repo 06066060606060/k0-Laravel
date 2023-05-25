58ba237c21b8522c490d4da2300487238a474980
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-26 00:36:21 +0200

    ok

--- lang/en/content.php
+++ lang/en/content.php
@@ -17,5 +17,17 @@ return [
     'c2' => 'Play Now',
     'c3' => 'Win Prizes',
     'c4' => '100% WINNING GAME!',
+    'c5' => 'Sign up for free',
+    'c6' => "Registration is quick, free, and we offer you 150 diamonds to get started.",
+    'c7' => 'Play your daily free games',
+    'c8' => "Every day, you have the opportunity to play 10 free games on the GoFRUITS multiplayer grid, with the chance to win instant prizes. Your scores give you the chance to participate in our monthly contest, where the grand prize of €500 (500 Coins) is up for grabs! This offer is an excellent opportunity to enjoy a fun game while having the chance to win great prizes.",
+    'c9' => 'Win Diamonds, Rubies, Coins',
+    'c10' => "Pool is an instant game grid that you can play to boost your chances of participating in the monthly contest. Plus, you have the opportunity to win Diamonds, Rubies, and Coins by playing this game! By accumulating your scores, you can improve your chances of winning the grand prize of €500 (500 Coins) offered in the monthly contest. This offer is a great opportunity for players to have fun while having the chance to win great rewards.",
+    'c11' => 'Convert them into gifts',
+    'c12' => "Our site offers a variety of attractive gifts, such as the Playstation 5, the Cookéo, the plancha, the soundbar, Rubies, as well as Amazon gift cards and cryptocurrency (crypto satoshi). Plus, you can withdraw your winnings for free via Paypal. This offer is an excellent opportunity for players to win incredible prizes while enjoying a fun and exciting gaming experience.",
+    'c13' => 'Free games',
+    'c14' => 'Our GoKDO site offers you the opportunity to play entertaining games for free and win gifts!',
+    'c15' => "Registration is free",
+    'c16' => "and you even get 150 Diamonds for free when you sign up. This offer is a great opportunity to have fun while having the chance to win interesting gifts. By playing on our site, you can enjoy an exciting gaming experience while increasing your chances of winning great prizes."
 
 ];
