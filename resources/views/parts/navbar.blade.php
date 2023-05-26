36d548f84887d0bc926acaaf124a1c28b8499542
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-26 02:18:52 +0200

    lol

--- resources/views/parts/navbar.blade.php
+++ resources/views/parts/navbar.blade.php
@@ -140,7 +140,7 @@
                                 <!-- Dropdown toggle button -->
                                 <button @click="isOpen = !isOpen"
                                     class="relative z-10 block p-2 mt-4 text-white bg-gray-800 border border-transparent rounded-md lg:mt-2 focus:border-blue-800 focus:ring-opacity-40 focus:ring-blue-300 focus:blue-400 focus:ring focus:outline-none">
-                                    <svg class="w-5 h-5 text-white" xmlns="http://www.w3.org/2000/svg"
+                                    lol<svg class="w-5 h-5 text-white" xmlns="http://www.w3.org/2000/svg"
                                         viewBox="0 0 20 20" fill="currentColor">
                                         <path fill-rule="evenodd"
                                             d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
