e0cf1f0147f30462b04d06997559aa6af3e02145
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-26 02:25:53 +0200

    ok

--- resources/views/parts/navbar.blade.php
+++ resources/views/parts/navbar.blade.php
@@ -136,7 +136,7 @@
                         class="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-blue-400 -rotate-12"></span>
                     <span class="relative">{{__('header.h13')}}</span>
                 </a>
-                <div x-data="{ isOpen: false }" class="relative inline-block mt-2">
+                <div x-data="{ isOpen: false }" class="relative inline-block mb-2">
                     <!-- Dropdown toggle button -->
                     <button @click="isOpen = !isOpen"
                         class="relative z-10 flex items-center p-2 mt-4 text-white bg-gray-800 border border-transparent rounded-md lg:mt-2 focus:border-blue-800 focus:ring-opacity-40 focus:ring-blue-300 focus:blue-400 focus:ring focus:outline-none">
