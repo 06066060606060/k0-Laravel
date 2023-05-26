8bfcb71fd906d4aec5c01e366c28a07e39a42410
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-26 02:24:56 +0200

    ok

--- resources/views/parts/navbar.blade.php
+++ resources/views/parts/navbar.blade.php
@@ -140,7 +140,7 @@
                     <!-- Dropdown toggle button -->
                     <button @click="isOpen = !isOpen"
                         class="relative z-10 flex items-center p-2 mt-4 text-white bg-gray-800 border border-transparent rounded-md lg:mt-2 focus:border-blue-800 focus:ring-opacity-40 focus:ring-blue-300 focus:blue-400 focus:ring focus:outline-none">
-                        <span class="mr-1">{{ LaravelLocalization::getCurrentLocaleNative() }}</span>
+                        <span class="mr-1">{{ ucfirst(LaravelLocalization::getCurrentLocaleNative()) }}</span>
                         <svg class="w-5 h-5 text-white" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                             <path fill-rule="evenodd"
                                 d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
@@ -159,7 +159,7 @@
                             <li>
                                 <a rel="alternate" hreflang="{{ $localeCode }}" href="{{ LaravelLocalization::getLocalizedURL($localeCode, null, [], true) }}"
                                     data-barba-prevent="self" class="block px-4 py-3 text-sm font-bold text-gray-300 capitalize transition-colors duration-300 transform hover:bg-gray-700 hover:text-white">
-                                    {{ $properties['native'] }}
+                                    {{ ucfirst($properties['native']) }}
                                 </a>
                             </li>
                         @endforeach
