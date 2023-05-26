99002dc808a4032d51d0dc53138cedc6273f58e5
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-26 13:13:10 +0200

    ok

---
+++ lang/es/header.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+
+/*
+|--------------------------------------------------------------------------
+| Authentication Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are used during authentication for various
+| messages that we need to display to the user. You are free to modify
+| these language lines according to your application's requirements.
+|
+*/
+
+'c1' => 'JUEGA GRATIS A',
+'c2' => 'Jugar Ahora',
+'c3' => 'Gana Regalos',
+'c4' => '¡JUEGO 100% GANADOR!',
+'c5' => 'Regístrate Gratis',
+'c6' => 'El registro es rápido, gratuito y te ofrecemos 150 diamantes para comenzar.',
+'c7' => 'Juega tus partidas gratuitas diarias',
+'c8' => 'Cada día tienes la oportunidad de jugar 10 partidas gratuitas en la cuadrícula GoFRUITS en multijugador, con la posibilidad de ganar premios instantáneos. Tus puntuaciones te permiten participar en nuestro concurso mensual, ¡donde el gran premio de 500€ (500 Coins) está en juego! Esta oferta es una excelente oportunidad para disfrutar de un juego divertido y tener la oportunidad de ganar fantásticos premios.',
+'c9' => 'Gana Diamantes, Rubíes, Coins',
+'c10' => 'Pool es una cuadrícula de juegos instantáneos que puedes jugar para aumentar tus posibilidades de participar en el concurso mensual. ¡Además, tienes la oportunidad de ganar Diamantes, Rubíes y Coins jugando a este juego! Acumulando tus puntuaciones, puedes mejorar tus posibilidades de ganar el gran premio de 500€ (500 Coins) ofrecido en el concurso mensual. Esta oferta es una excelente oportunidad para que los jugadores se diviertan y tengan la posibilidad de ganar fantásticas recompensas.',
+'c11' => 'Cámbialos por regalos',
+'c12' => 'Nuestro sitio ofrece una variedad de regalos atractivos, como la Playstation 5, el Cookéo, la plancha, la barra de sonido, los Rubíes, así como tarjetas de regalo de Amazon y criptomonedas (crypto satoshi). Además, puedes retirar tus ganancias a través de Paypal de forma gratuita. Esta oferta es una excelente oportunidad para que los jugadores ganen premios increíbles mientras disfrutan de una experiencia de juego divertida y emocionante.',
+'c13' => 'Juegos gratuitos',
+'c14' => '¡Nuestro sitio GoKDO te ofrece la oportunidad de jugar juegos divertidos de forma gratuita y ganar regalos!',
+'c15' => 'La inscripción es gratuita',
+'c16' => 'y incluso recibes 150 Diamantes gratis al registrarte. Esta oferta es una excelente oportunidad para divertirte y tener la oportunidad de ganar regalos interesantes. Al jugar en nuestro sitio, puedes disfrutar de una emocionante experiencia de juego y aumentar tus posibilidades de ganar fantásticos premios.',
+'c17' => 'Juego de Evento',
+'c18' => 'Juegos Multijugador',
+'c19' => 'Jugar',
+'c20' => 'por 24h',
+'c21' => 'ÚLTIMOS GANADORES',
+
+];
