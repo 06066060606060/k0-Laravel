037a838d5c982d9abf39eab945c85e93f7522bbb
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-28 23:37:08 +0200

    ok

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\StripePaymentController;
 */
 Route::group([
     'prefix' => LaravelLocalization::setLocale(),
-    'middleware' => ['localeSessionRedirect', 'localizationRedirect']
+    'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
 ], function() {
 Route::controller(GlobalController::class)->group(function(){
 // Route::get('/', 'getAll')->name('getAll')->middleware('App\Http\Middleware\MyMiddleware');
