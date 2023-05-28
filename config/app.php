0e89aebc283a67a0739afccbce31956801018881
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-28 05:45:59 +0200

    ok

--- config/app.php
+++ config/app.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Facade;
-use Mcamara\LaravelLocalization\LaravelLocalization;
 
 return [
 
@@ -57,7 +56,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'asset_url' => env('APP_URL') . '/' . LaravelLocalization::getCurrentLocale() . env('ASSET_URL'),
+'asset_url' => env('APP_URL').env('ASSET_URL'),
 
     /*
     |--------------------------------------------------------------------------
