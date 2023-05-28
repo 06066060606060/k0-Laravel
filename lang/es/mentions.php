f50ca5f8a03912e212b29aed3bc6710d71928932
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-28 04:55:13 +0200

    ok

--- lang/es/mentions.php
+++ lang/es/mentions.php
@@ -6,7 +6,7 @@ return [
     'ml3' => "Denominación o razón social",
     'ml4' => "Nombre del responsable de la redacción/gestión del sitio",
     'ml5' => "Estructura jurídica",
-    'ml6' => "Hosting",
+    'ml6' => "Hosting", 
     'ml7' => "El sitio web GoKDO está alojado en HOSTINGER, cuya sede social se encuentra en HOSTINGER INTERNATIONAL LTD, 61 Lordou Vironos Street, 6023 Larnaca, Chipre, accesible a través de los siguientes medios: https://www.hostinger.fr/contact.",
     'ml8' => "Creación",
     'ml9' => "Sitio web desarrollado y diseñado por Micky XBMOD",
