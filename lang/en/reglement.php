dcd1b7b7b63f93fa4ef0220714ae80e1e685cb97
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-28 04:44:01 +0200

    ok

---
+++ lang/en/reglement.php
@@ -0,0 +1,40 @@
+<?php 
+return [
+
+    'r1' => 'Rules',
+    'r2' => 'Gokdo.com Rules',
+    'r3' => 'Access to Gokdo.com',
+    'r4' => "Gokdo.com is accessible through a Google Play application or by visiting the website https://gokdo.com. It is a free gaming site with no purchase obligation.",
+    'r6' => 'The GoFRUITS Game',
+    'r7' => "Players receive 10 free games every day on the multiplayer game GoFRUITS.
+    An exchange of 10 additional games on the GoFRUITS game is worth 5 Rubies. The goal of the GoFRUITS game is to find the highest win. Once the highest win is found, the game grid is regenerated, and some squares in the GoFRUITS grid are losing squares.",
+    'r8' => 'The Pool Game',
+    'r9' => "It takes 10 Rubies to play the Pool game. The multiplayer Pool game is 100% winning, with no losing squares. The goal of the Pool game is to find the highest win. If the highest win is discovered, a new grid is generated.",
+    'r10' => 'Monthly Contests',
+    'r11' => "Every month, a contest is held on Gokdo for a specified duration that may be modified at any time. Each contest promises prizes to the top 3000 players in the ranking, and they will receive a prize, whether it's an Amazon gift card, PayPal payment, Coins, Rubies, or Diamonds. From the 3001st player onwards, they won't win anything.",
+    'r12' => 'Purchases on Gokdo.com',
+    'r13' => "Members who wish to do so can make a purchase via PayPal or credit card in the pack section of the website menu, which allows the purchase of additional Rubies. Gokdo remains a free gaming site with no purchase obligation.",
+    'r14' => 'Cheating and Fraudulent Manipulation',
+    'r15' => "Any cheating or fraudulent manipulation of the site or the game will result in the deletion of the member's account without prior notice or refund.",
+    'r16' => 'Gift Page',
+    'r17' => "A gift page is available and offers gifts in exchange for Diamonds or Coins. The gifts are diverse and varied, classified into categories and prices. Orders are dispatched within a maximum of 30 days. The webmaster can delete orders from players who have violated the rules.",
+    'r18' => "Contacting the Gokdo.com Team",
+    'r19' => "Players can contact the Gokdo team once a day via the contact area, the link to which is located at the bottom of the page.",
+    'r5' => "Emails can be sent by the administrator to the member, and members can contact the webmaster to request the cessation of email communications.",
+    'r20' => 'Game Events',
+    'r21' => "Depending on the time of year, game events may be organized on Gokdo. These events may take the same form as the monthly contest or be different. The specific rules for each event will be indicated on Gokdo's dedicated page.",
+    'r22' => 'Intellectual Property',
+    'r23' => "All elements present on the Gokdo website, such as texts, images, graphics, logos, icons, sounds, and software, are the exclusive property of Gokdo or its partners and are protected by French and international laws on copyright and intellectual property.",
+    'r32' => "Any reproduction, representation, modification, publication, transmission, distortion, in whole or in part, of the elements of the website, regardless of the means or process used, is prohibited without the prior written authorization of Gokdo.",
+    'r33' => "Any unauthorized use of these elements engages the user's responsibility and constitutes a violation of copyright and intellectual property rights.",
+    'r24' => 'Liability',
+    'r25' => "Gokdo cannot be held responsible for any malfunction of the website, disruption of access to the website, fraudulent or abusive use of the website, loss or alteration of data, or any other direct or indirect damage resulting from the use of the website or the games offered on the website. Gokdo cannot be held responsible for the misuse of the games offered on the website or any fraudulent or inappropriate behavior by the members.",
+    'r26' => 'Modification of the Rules',
+    'r27' => 'Gokdo reserves the right to modify the present rules at any time and without prior notice. Members will be informed of any modifications through a message displayed on the website or by email.',
+    'r28' => 'Disputes and Applicable Law',
+    'r29' => 'In the event of a dispute, an amicable solution will be sought first. Failing that, the French courts shall have sole jurisdiction. The present rules are subject to French law.',
+    'r30' => 'Contact',
+    'r31' => 'For any questions regarding these rules, members can contact Gokdo using the contact form available on the website.',
+
+
+];
