70626b59708f83264a0f748f180401ae7cffbd5f
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-29 02:53:22 +0200

    ok

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -5,8 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
-use App\Models\Session as AppSession; // Rename the conflicting Session class
-use Illuminate\Support\Facades\Session; // Use full namespace for Session facade
+use Illuminate\Support\Facades\Session;
 
 class Localization
 {
