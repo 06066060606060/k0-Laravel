81093438e3d3d041450f3d7f9468569b4d6485e2
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-29 03:07:25 +0200

    ok

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\App;
 
 use App\Models\Cadeaux;
 use App\Models\Commandes;
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Middleware\Cors;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MailController;
 use App\Http\Controllers\GlobalController;
