2b9aeda2f2572ea020991421e7a7eea54a5cf5a5
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-30 19:37:56 +0200

    ok

--- resources/views/winner.blade.php
+++ resources/views/winner.blade.php
@@ -28,9 +28,9 @@
                 </div>
                 <div
                     class="flex flex-col items-start mt-12 mb-16 text-left lg:flex-grow lg:w-1/2 lg:pl-6 xl:pl-24 md:mb-0 xl:mt-0">
-                    <span class="mb-1 font-bold tracking-widest text-blue-600 uppercase text-md"> Concours:</span>
+                    <span class="mb-1 font-bold tracking-widest text-blue-600 uppercase text-md"> {{__('Concours')}}:</span>
                     <h1 class="mb-2 text-gray-100">
-                      Du  {{ $startdate }}  au  {{ $enddate }} </h1>
+                      {{__('Du')}}  {{ $startdate }}  {{__('au')}}  {{ $enddate }} </h1>
                     <h1
                         class="mb-4 text-4xl font-bold leading-none tracking-tighter text-gray-100 md:text-7xl lg:text-5xl">
                       {{ $concours->name }} </h1>
@@ -47,7 +47,7 @@
                                     class="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-indigo-600 -rotate-12"></span>
                                 <span
                                     class="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-indigo-400 -rotate-12"></span>
-                                <span class="relative">Jouer</span>
+                                <span class="relative">{{__('Jouer')}}</span>
                             </a>
                     </div>
 
@@ -62,7 +62,7 @@
          <container class="flex flex-col min-h-screen px-8 mx-auto md:flex-row lg:max-w-6xl md:pl-16">
              <div class="container px-5 pt-8 mx-auto">
                  <div class="flex flex-col w-full mb-20 text-center">
-                     <h1 class="mb-4 text-4xl font-bold text-gray-300 md:text-5xl title-font">Classement Concours:</h1>
+                     <h1 class="mb-4 text-4xl font-bold text-gray-300 md:text-5xl title-font">{{__('Classement Concours')}}:</h1>
                      <section class="text-gray-300 body-font">
                          <div class="container py-8 mx-auto">
                                  <div
@@ -83,18 +83,18 @@
                                                  <th  style="width:30%;"
                                                      class="px-4 py-2 font-bold text-left text-white whitespace-nowrap">
                                                      <div class="flex items-center gap-2">
-                                                         Pseudo
+                                                         {{__('Pseudo')}}
                                                      </div>
                                                  </th>
                                                  <th  style="width:30%;"
                                                      class="px-4 py-2 font-bold text-left text-white whitespace-nowrap">
                                                      <div class="flex items-center gap-2">
-                                                         Score
+                                                         {{__('Score')}}
                                                      </div>
                                                  </th>
                                                  <th  style="width:30%;"
                                                      class="px-4 py-2 font-bold text-left text-white whitespace-nowrap">
-                                                     Gain
+                                                     {{__('Gain')}}
                                                  </th>
                                              </tr>
                                          </thead>
@@ -205,13 +205,13 @@
          </container>
      </div>
      @else
-<div class="text-gray-200 mt-12 mb-10 text-center text-3xl">Pas de concours actuellement.</div><br>
+<div class="text-gray-200 mt-12 mb-10 text-center text-3xl">{{__('Pas de concours actuellement.')}}</div><br>
 <!-- WINNER -->
 <winner class="mx-auto max-w-7xl" id="win">
     <section>
         <div class="mb-4 px-2 py-2 mx-8 bg-gray-800 rounded-lg lg:mx-8 xl:mx-auto bg-opacity-40 max-w-7xl sm:px-16 md:px-24 lg:py-18">
             <h2 class="text-2xl font-bold tracking-tight text-center text-gray-100 ">
-                GAGNANTS DERNIERS CONCOURS
+                {{__('GAGNANTS DERNIERS CONCOURS')}}
             </h2> 
 
             <div class="pb-4 mt-4 border-gray-600 md:mt-4 swiper-container swiper-initialized swiper-horizontal swiper-backface-hidden">
