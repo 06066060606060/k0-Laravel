f1818999da349e6d3f0cb9129352a70790a97314
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-05-30 19:40:09 +0200

    ok

--- resources/views/winner.blade.php
+++ resources/views/winner.blade.php
@@ -211,7 +211,7 @@
     <section>
         <div class="mb-4 px-2 py-2 mx-8 bg-gray-800 rounded-lg lg:mx-8 xl:mx-auto bg-opacity-40 max-w-7xl sm:px-16 md:px-24 lg:py-18">
             <h2 class="text-2xl font-bold tracking-tight text-center text-gray-100 ">
-                {{__('GAGNANTS DERNIERS CONCOURS')}}
+                {{__('DERNIERS GAGNANTS CONCOURS')}}
             </h2> 
 
             <div class="pb-4 mt-4 border-gray-600 md:mt-4 swiper-container swiper-initialized swiper-horizontal swiper-backface-hidden">
