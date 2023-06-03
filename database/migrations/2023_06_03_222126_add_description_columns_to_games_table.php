0835aaeec566c3548611a01dabef026661e8b0bb
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-04 00:21:54 +0200

    ok

---
+++ database/migrations/2023_06_03_222126_add_description_columns_to_games_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            $table->string('description_en', 255);
+            $table->string('description_de', 255);
+            $table->string('description_es', 255);
+            $table->string('description_it', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('games', function (Blueprint $table) {
+            //
+        });
+    }
+};
