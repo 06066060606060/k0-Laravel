e9779863527cab0d753a2037d28aff7dcb2f1398
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-04 01:15:57 +0200

    ok

--- app/Models/Packs.php
+++ app/Models/Packs.php
@@ -19,7 +19,7 @@ class Packs extends Model
     // protected $primaryKey = 'id';
     public $timestamps = false;
     protected $guarded = ['id'];
-   protected $fillable = ['name', 'image', 'description', 'prix', 'promo', 'active', 'prix_promo', 'gain', 'type'];
+   protected $fillable = ['name', 'image', 'description', 'description_en', 'description_de', 'description_es', 'description_it', 'prix', 'promo', 'active', 'prix_promo', 'gain', 'type'];
     // protected $hidden = [];
     // protected $dates = [];
 
