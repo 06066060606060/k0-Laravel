47acac1c1487d0d443946bfff29790578d3e8e62
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-06 00:38:04 +0200

    ok

--- resources/views/winner.blade.php
+++ resources/views/winner.blade.php
@@ -111,7 +111,7 @@
                                          </thead>
 
                                          <tbody class="divide-y divide-gray-700">
-@if(empty($lesscoresdeconcours))
+@if($lesscoresdeconcours <= 0)
 <tr class="dark:bg-gray-900">
     <td class="px-4 py-2 text-center text-white whitespace-nowrap" colspan="4">
     Pas de participant actuellement
