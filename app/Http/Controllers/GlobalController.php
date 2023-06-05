0385ee7ecaffd3b98e43ebffdee84b83ffeb7ee6
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-05 22:30:53 +0200

    ok

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -20,7 +20,6 @@ use Pestopancake\LaravelBackpackNotifications\Notifications\DatabaseNotification
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\DB;
 
 class GlobalController extends Controller
 {
@@ -260,10 +259,7 @@ class GlobalController extends Controller
                     }
                 }
                 Concours::where('id', 10)->update(['active' => 0]);
-                DB::statement('SET FOREIGN_KEY_CHECKS=0');
-                ScoresConcours::truncate();    
-                DB::statement('SET FOREIGN_KEY_CHECKS=1');
-
+                ScoresConcours::query()->delete();
             }
             return view('winner', compact('lesderniers_gagnants_concours', 'derniers_gagnants_concours', 'gain_nom', 'gain', 'gains', 'position', 'scoresconcours', 'concours', 'startdate', 'enddate', 'gain_nom'));
         } else {
