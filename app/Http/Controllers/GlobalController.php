6584bd46a3a504132f0d740e58ac51e6504cc8c3
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-06 00:11:47 +0200

    ok

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -130,7 +130,7 @@ class GlobalController extends Controller
         $now = Carbon::now(); // Vérifie si date actuelle est après date de fin du concours
         
         //Score effectués ordre par id desc
-        if (isset($concours->active==1)) {
+        if (isset($concours->active=='1')) {
             $scoresconcours = ScoresConcours::selectRaw('id_user, SUM(score) AS total')
                 ->where('game_id', $concours->game_id) // ou id du jeu = au jeu du concours
                 ->groupBy('id_user') // groupé par id users
