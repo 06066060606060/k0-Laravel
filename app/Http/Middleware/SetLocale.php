c3286159459b7a812f8e8c36ae5f58d319a61a17
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-05 12:58:59 +0200

    ok

---
+++ app/Http/Middleware/SetLocale.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\App;
+
+// ...
+
+public function handle($request, Closure $next)
+{
+    if ($request->session()->has('locale')) {
+        $locale = $request->session()->get('locale');
+        if (in_array($locale, LaravelLocalization::getSupportedLocales())) {
+            App::setLocale($locale);
+        }
+    }
+
+    return $next($request);
+}
