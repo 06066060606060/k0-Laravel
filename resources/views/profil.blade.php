0ee34aad89dd7b5cbba6e35b61a72f3b33a9d283
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-07 20:59:53 +0200

    ok

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -313,7 +313,7 @@
                                 <div class="col-span-full sm:col-span-2">
                                     <label for="state" class="text-sm text-gray-300">{{__('Pays')}}</label>
                                     <select id="state" name="state" class="w-full px-2 py-2 text-gray-900 border-gray-700 rounded-md focus:ring focus:ring-opacity-75 focus:ring-blue-400">
-    @if(empty($info))
+    @if(empty($infos))
     <option value="" selected>{{__("Choisissez votre pays")}}</option>
 <option value="Afghanistan">{{__("Afghanistan")}}</option>
 <option value="Afrique du Sud">{{__("Afrique du Sud")}}</option>
