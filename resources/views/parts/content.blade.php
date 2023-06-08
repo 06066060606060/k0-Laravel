afd908d12cbdb193aee1def60ba946e7f87960a5
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-08 15:03:33 +0200

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -497,7 +497,7 @@
                                 <p class="text-xs leading-relaxed text-gray-800 md:text-sm">{{ $sologame->description_it }}</p>
                                 @else
                                 @endif
-                                    <a href="game?id={{ $sologame->id }}" onclick="event.preventDefault(); window.location.reload(true); window.location.href='game?id={{ $allgame->id }}';"
+                                    <a href="game?id={{ $sologame->id }}" onclick="event.preventDefault(); window.location.reload(true); window.location.href='game?id={{ $sologame->id }}';"
                                         class="relative flex justify-center w-24 px-5 py-2 mx-auto mt-4 font-medium text-white shadow-lg group">
                                         <span
                                             class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-indigo-500 group-hover:bg-indigo-700 group-hover:skew-x-12"></span>
