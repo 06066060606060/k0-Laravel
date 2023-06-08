e43162fcec56b266bfac2bc94704bfc6938a7355
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-08 23:26:42 +0200

    ok

---
+++ database/migrations/2023_06_08_212618_add_bingo_columns_to_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('parties_bingo')->default(1)->after('parties_bonus_gowin');
+            $table->integer('parties_bonus_bingo')->default(0)->after('parties_bingo');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
