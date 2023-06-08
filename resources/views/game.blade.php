55f37ef1abe780e9d27fd352c95e87b14c88c0c4
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-09 01:10:03 +0200

    ok

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -69,9 +69,12 @@
                               $link =  $game->link ?? null;
                               $secret =  encrypt(['userid' => $userid, '&tk=' . csrf_token(), 'rubis' => $rubis, 'gameid' => $game->id, 'free_game' => $free, 'parties' => $parties, 'timestamp' => time()]);
                              @endphp
-                             <div><iframe id="gameBody" src="{{ $link . '?userid=' . $userid . '&locale=' . app()->getLocale() . '&tk=' . csrf_token() . '&user_name=' . $username . '&rubis=' . $rubis . '&gameid=' . $game->id . '&free_game=' . $free . '&parties=' . $parties . '&secret=' . $secret}}" class="w-full h-[667px] overflow-hidden -mt-1"
-                                 scrolling="no"></iframe></div>
-                                 <div><button id="fullscreenButton">JOUER EN MODE PLEIN ECRAN</button></div>
+                             <div>
+                            <iframe id="gameBody" src="{{ $link . '?userid=' . $userid . '&locale=' . app()->getLocale() . '&tk=' . csrf_token() . '&user_name=' . $username . '&rubis=' . $rubis . '&gameid=' . $game->id . '&free_game=' . $free . '&parties=' . $parties . '&secret=' . $secret}}" class="w-full h-[667px] overflow-hidden -mt-1" scrolling="no"></iframe>
+                            </div>
+                            <div>
+                            <button id="fullscreenButton">JOUER EN MODE PLEIN ECRAN</button>
+                            </div>
 
                                 
                                 @else
