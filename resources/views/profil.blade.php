005f4399858338e2755eaa2da6e5ba0858949e7e
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-21 00:04:14 +0200

    ok

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -301,7 +301,7 @@
                                         class="w-full px-2 py-2 text-gray-900 border-gray-700 rounded-md focus:ring focus:ring-opacity-75 focus:ring-blue-400"
                                         value="{{ $infos[0]->prenom ?? null }}">
                                 </div>
-                                <hr>
+                                <hr class="col-span-full">
                                 <div class="col-span-full">
                                 Votre date de naissance</div>
                                 <div class="col-span-full sm:col-span-2">
