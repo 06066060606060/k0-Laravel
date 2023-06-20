6f0299264168af625bf44cb8724f51f17a7f0d55
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-21 00:09:35 +0200

    ok

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -301,7 +301,7 @@
                                         class="w-full px-2 py-2 text-gray-900 border-gray-700 rounded-md focus:ring focus:ring-opacity-75 focus:ring-blue-400"
                                         value="{{ $infos[0]->prenom ?? null }}">
                                 </div>
-                                <hr class="col-span-full">
+                                <!--<hr class="col-span-full">
                                 <div class="col-span-full font-bold text-gray-300">
                                 Votre date de naissance</div>
                                 <div class="col-span-full sm:col-span-2">
@@ -331,7 +331,7 @@
                         @endfor
                     </select>
                 </div>
-                                <hr class="col-span-full">
+                                <hr class="col-span-full">-->
 
                                 <div class="col-span-full">
                                     <label for="address" class="text-sm text-gray-300">{{__('Adresse')}}</label>
