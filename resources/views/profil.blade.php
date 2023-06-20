28f584d9cfe8a2c9be9ae7b4a2537c73a6a38437
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-21 00:02:38 +0200

    ok

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -301,6 +301,8 @@
                                         class="w-full px-2 py-2 text-gray-900 border-gray-700 rounded-md focus:ring focus:ring-opacity-75 focus:ring-blue-400"
                                         value="{{ $infos[0]->prenom ?? null }}">
                                 </div>
+                                <hr>
+                                <div>Votre date de naissance</div>
                                 <div class="col-span-full sm:col-span-2">
                     <label for="day" class="text-sm text-gray-300">{{__('Jour')}}</label>
                     <select id="day" name="day"
