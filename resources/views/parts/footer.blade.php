c4d5d888b50c628a3925d0d48ede23c2a0c079ee
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-21 17:02:35 +0200

    ok

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -57,7 +57,7 @@
                          class="w-5 h-5" viewBox="0 0 24 24">
                          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                      </svg>
-                    @if($isMobile==true)
+                    @if($isMobile == true)
                     Facebook
                     @else
                     @endif
