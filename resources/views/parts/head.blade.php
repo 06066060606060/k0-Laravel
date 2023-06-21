9a1ae4efaca6589d72a765080a979e72601e8214
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-21 16:31:50 +0200

    ok

--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -22,8 +22,8 @@
   <link rel="canonical" href="https://gokdo.com" />
 
 
-  <script src="https://kit.fontawesome.com/59ecaaffaa.js" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://kit.fontawesome.com/59ecaaffaa.js" crossorigin="anonymous" defer></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" defer></script>
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
   <link crossorigin="anonymous" href="https://unpkg.com/swiper/swiper-bundle.min.css" rel="stylesheet" />
