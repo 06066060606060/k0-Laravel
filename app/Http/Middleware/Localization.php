59254d8f79bd1d506a0bf9fa93bc0e93ff682d89
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-21 18:29:30 +0200

    ok

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use App\Models\Session as AppSession; // Rename the conflicting Session class
 use Illuminate\Support\Facades\Session; // Use full namespace for Session facade
+use Negotiation\LanguageNegotiator;
 
 class Localization
 {
@@ -18,25 +19,31 @@ class Localization
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
-{
-    $availableLocales = config('app.available_locales');
-    
-    // Vérifier si la langue est déjà définie dans la session
-    if (Session::has('locale') && in_array(Session::get('locale'), $availableLocales)) {
-        App::setLocale(Session::get('locale'));
-    } else {
-        // Récupérer la langue préférée du navigateur
-        $browserLocale = $request->getPreferredLanguage($availableLocales);
-
-        // Définir la langue du navigateur comme langue par défaut si elle est disponible
-        if (in_array($browserLocale, $availableLocales)) {
-            App::setLocale($browserLocale);
+    {
+        $availableLocales = config('app.available_locales');
+        
+        // Vérifier si la langue est déjà définie dans la session
+        if (Session::has('locale') && in_array(Session::get('locale'), $availableLocales)) {
+            App::setLocale(Session::get('locale'));
         } else {
-            // Utiliser la langue par défaut de l'application si la langue du navigateur n'est pas disponible
-            App::setLocale(config('app.fallback_locale'));
-        }
-    }
+            // Récupérer les langues préférées du navigateur
+            $negotiator = new LanguageNegotiator();
+            $browserLocales = $negotiator->getAcceptedLanguages($request->header('Accept-Language'));
     
-    return $next($request);
-}
+            // Chercher la première langue préférée du navigateur disponible dans la configuration
+            foreach ($browserLocales as $browserLocale) {
+                if (in_array($browserLocale, $availableLocales)) {
+                    App::setLocale($browserLocale);
+                    break;
+                }
+            }
+    
+            // Utiliser la langue par défaut de l'application si aucune langue du navigateur n'est disponible
+            if (!App::getLocale()) {
+                App::setLocale(config('app.fallback_locale'));
+            }
+        }
+        
+        return $next($request);
+    }    
 }
