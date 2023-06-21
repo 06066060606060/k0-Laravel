9df4333d10fef54aa88bd9117672d3305c9e981f
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-21 17:31:52 +0200

    ok

--- resources/views/vendor/backpack/base/inc/head.blade.php
+++ resources/views/vendor/backpack/base/inc/head.blade.php
@@ -32,7 +32,7 @@
 
     @yield('after_styles')
     @stack('after_styles')
-    <script src="https://gokdo.com/build/assets/app.fd7ac64b.js" async></script>
+    @vite('resources/js/app.js')
     {{-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries --}}
     {{-- WARNING: Respond.js doesn't work if you view the page via file:// --}}
     <!--[if lt IE 9]>
