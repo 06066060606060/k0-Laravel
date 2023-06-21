7147885039f419c7132d0aff3ecf3e1bb477560b
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-21 23:16:12 +0200

    ok

--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -24,7 +24,6 @@
   //fontawesome
   <script src="resources\js\fontawesome.js" crossorigin="anonymous" defer></script>
   
-  <script src="https://kit.fontawesome.com/59ecaaffaa.js" crossorigin="anonymous" defer></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" async></script>
   <style>
   @charset "UTF-8";
