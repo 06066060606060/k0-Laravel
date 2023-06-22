76251ad1eccf3b3e9cc296bcc81ada5dd8f32522
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-23 00:51:59 +0200

    ok

--- app/Http/Controllers/ExtendedRegisterController.php
+++ app/Http/Controllers/ExtendedRegisterController.php
@@ -10,42 +10,72 @@ use Backpack\CRUD\app\Http\Controllers\Auth\RegisterController;
 
 class ExtendedRegisterController extends RegisterController
 {
-    use RegistersUsers;
+    protected $data = []; // the information we send to the view
 
+    use RegistersUsers;
+    public function __construct()
+    {
+        $guard = backpack_guard_name();
+
+        $this->middleware("guest:$guard");
+
+        // Where to redirect users after login / registration.
+        $this->redirectTo = property_exists($this, 'redirectTo') ? $this->redirectTo
+            : config('backpack.base.route_prefix', 'dashboard');
+    }
     public function validator(array $data)
     {
-        $validator = Validator::make($data, [
-            // Les règles de validation de base pour l'inscription
-            // ...
+        $user_model_fqn = config('backpack.base.user_model_fqn');
+        $user = new $user_model_fqn();
+        $users_table = $user->getTable();
+        $email_validation = backpack_authentication_column() == 'email' ? 'email|' : '';
+
+        return Validator::make($data, [
+            'name'                             => 'required|max:255',
+            backpack_authentication_column()   => 'required|'.$email_validation.'max:255|unique:'.$users_table,
+            'password'                         => 'required|min:6|confirmed',
             'parrain' => ['nullable', 'exists:users,id'],
         ]);
-
-        return $validator;
     }
 
     protected function create(array $data)
     {
-        event(new Registered($user = $this->register($data)));
+        $user_model_fqn = config('backpack.base.user_model_fqn');
+        $user = new $user_model_fqn();
 
-        $parrain = $data['parrain'] ?? null;
-
-        // Faites le traitement du champ "parrain" ici, par exemple :
-        if ($parrain) {
-            // Associez le parrain à l'utilisateur créé.
-        }
-
-        return $user;
+        return $user->create([
+            'name'                             => $data['name'],
+            backpack_authentication_column()   => $data[backpack_authentication_column()],
+            'password'                         => bcrypt($data['password']),
+            'parrain'                          => $data['parrain'];
+        ]);
     }
 
     public function register(Request $request)
     {
+        // if registration is closed, deny access
+        if (! config('backpack.base.registration_open')) {
+            abort(403, trans('backpack::base.registration_closed'));
+        }
+
         $this->validator($request->all())->validate();
 
-        event(new Registered($user = $this->create($request->all())));
+        $user = $this->create($request->all());
 
+        event(new Registered($user));
         $this->guard()->login($user);
 
         return redirect($this->redirectPath());
+    }
+
+    /**
+     * Get the guard to be used during registration.
+     *
+     * @return \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected function guard()
+    {
+        return backpack_auth();
     }
 
 
