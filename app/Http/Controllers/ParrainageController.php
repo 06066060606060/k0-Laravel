664909a1c488ad19b16f5c8631eb76d0fad63d23
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-22 18:55:27 +0200

    ok

--- app/Http/Controllers/ParrainageController.php
+++ app/Http/Controllers/ParrainageController.php
@@ -8,20 +8,24 @@ use Illuminate\Support\Facades\Session;
 class ParrainageController extends Controller
 {
     public function setParrainageLink(Request $request, $le_parrain)
-    {
-        // Vérifier si le parrain existe dans la table "users"
-        $parrainExiste = User::where('name', $le_parrain)->exists();
-    
-        if ($parrainExiste) {
-            // Stocker le parrain dans la session
-            Session::put('parrain', $le_parrain);
-    
-            // Rediriger vers la page d'inscription avec le parrain comme paramètre
-            return redirect()->route('backpack.auth.register', ['parrain' => $le_parrain]);
-        } else {
-            // Rediriger vers la page d'inscription sans le paramètre de parrain
-            return redirect()->route('backpack.auth.register');
-        }
+{
+    // Vérifier si le parrain existe dans la table "users"
+    $parrainExiste = User::where('name', $le_parrain)->exists();
+
+    if ($parrainExiste) {
+        // Stocker le parrain dans la session
+        Session::put('parrain', $le_parrain);
+
+        // Rediriger vers la page d'inscription avec le parrain comme paramètre
+        return redirect()->route('backpack.auth.register', ['parrain' => $le_parrain]);
+    } elseif ($le_parrain === '') {
+        // Si aucun parrain n'est spécifié, rediriger vers la page d'inscription sans le paramètre de parrain
+        return redirect()->route('backpack.auth.register');
+    } else {
+        // Si le parrain n'existe pas en base de données, afficher un message d'erreur ou rediriger vers une autre page
+        return redirect()->route('backpack.auth.register');
     }
+}
+
     
 }
