637d0a91de9e200551a9a571a2b555f31c4a094b
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-22 15:23:27 +0200

    ok

--- app/Http/Controllers/ParrainageController.php
+++ app/Http/Controllers/ParrainageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class ParrainageController extends Controller
 {
@@ -11,9 +12,7 @@ class ParrainageController extends Controller
         // Stockez le nom d'utilisateur dans le localstorage ou tout autre moyen de stockage approprié
         // Voici un exemple d'utilisation de la méthode "put" pour stocker dans le localstorage
         
-        // Assurez-vous d'inclure la bibliothèque Illuminate\Support\Facades\Session en haut du fichier
-        use Illuminate\Support\Facades\Session;
-        
+        // Assurez-vous d'inclure la bibliothèque Illuminate\Support\Facades\Session en haut du fichier        
         Session::put('parrain', $le_parrain);
         
         // Redirigez vers la page d'inscription ou toute autre page pertinente
