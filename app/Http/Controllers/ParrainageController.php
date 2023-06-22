c01654d132ea5a465ecce78af06b1feded880169
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-22 15:14:19 +0200

    ok

---
+++ app/Http/Controllers/ParrainageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ParrainageController extends Controller
+{
+    public function setParrainageLink(Request $request, $le_parrain)
+    {
+        // Stockez le nom d'utilisateur dans le localstorage ou tout autre moyen de stockage approprié
+        // Voici un exemple d'utilisation de la méthode "put" pour stocker dans le localstorage
+        
+        // Assurez-vous d'inclure la bibliothèque Illuminate\Support\Facades\Session en haut du fichier
+        use Illuminate\Support\Facades\Session;
+        
+        Session::put('parrain', $le_parrain);
+        
+        // Redirigez vers la page d'inscription ou toute autre page pertinente
+        return redirect()->route('inscription');
+    }
+}
