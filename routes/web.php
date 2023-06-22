02aae53ab09ac865fb7cd6494d9de9aa230e02b7
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-23 00:25:30 +0200

    ok

--- routes/web.php
+++ routes/web.php
@@ -10,16 +10,6 @@ use App\Http\Controllers\ParrainageController;
 use App\Http\Controllers\StripePaymentController;
 use App\Controllers\ExtendedRegisterController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 Route::get('admin/register', [ExtendedRegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('admin/register', [ExtendedRegisterController::class, 'register']);
 
