4cf54f2e08a3f7debdbd31ed814a21cd564b4491
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-26 01:43:14 +0200

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -105,9 +105,9 @@
     <tbody>
         <tr>
             <td class="pr-4">
-                <i class="fas fa-user-group text-white text-4xl"></i>
+                <i class="fas fa-3x fa-user-group text-white"></i>
             </td>
-            <td class="pl-4 text-white">{{__('Remportez')}} 20 <img src='img/gem10.png' class='display-inline w-6 h-3 align-middle' alt='Gem 10'> {{__('par ami parrainé!')}}<br>
+            <td class="pl-4 text-white"><div class="display-inline">{{__('Remportez')}} 20 </div><img src='img/gem10.png' class='display-inline w-6 h-3 align-middle' alt='Gem 10'> {{__('par ami parrainé!')}}<br>
             {{__('Votre lien de parrainage :')}} https://gokdo.com/admin/register?parrain={{ $lejoueur }}<br>
             <i>{{__("Toute triche sera synonyme d'exclusion du site")}}</i></td>
         </tr>
