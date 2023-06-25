33ebfac7a3ce6868510cdb172f37196d8bd87b3b
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-26 01:56:48 +0200

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -98,7 +98,7 @@
             <div class="flex flex-col w-full text-center">
                 <h1 class="mb-4 text-4xl font-bold text-gray-100 md:text-4xl title-font">{{__('Parrainage')}}</h1>
             </div>            
-            <table class="mt-2 mx-auto m-full">
+            <table class="mt-2 mx-auto m-full text-s">
                 <tbody>
                     <tr>
                         <td class="pr-4">
