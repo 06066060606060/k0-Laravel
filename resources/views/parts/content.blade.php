7cffa35a44386290be8fa32538c30176c480ed55
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-29 00:37:55 +0200

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -131,7 +131,7 @@
 @else
             <p class="py-0 ml-4 mr-2 text-gray-200 lg:ml-8 md:pb-0 marquee">
 @endif
-   <i class="fas fa-solid fa-rocket pr-2 fa-lg" style="color: #005eff;"></i> {!! __("<b>Le saviez-vous :</b> Les <img src='img/gem10.png' class='w-5 h-5 inline-block'> vous permettent de jouer plus de parties, vous pouvez en acheter via le lien Pack de notre menu.") !!}</p>
+   <i class="fas fa-solid fa-rocket pr-2 fa-lg" style="color: yellow;"></i> {!! __("<b>Le saviez-vous :</b> Les <img src='img/gem10.png' class='w-5 h-5 inline-block'> vous permettent de jouer plus de parties, vous pouvez en acheter via le lien Pack de notre menu.") !!}</p>
 
         </div>
     </div>
