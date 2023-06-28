a2e8ce5446c0abb7f4f4f444dc46a60969f2935a
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-29 00:27:14 +0200

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -129,7 +129,7 @@
 @if($isMobile == true)
             <p class="py-2 ml-4 mr-2 font-bold text-gray-200 lg:ml-8 md:pb-0 marquee">
 @else
-            <p class="py-0 ml-4 mr-2 font-bold text-gray-200 lg:ml-8 md:pb-0 marquee">
+            <p class="py-0 ml-4 mr-2 text-gray-200 lg:ml-8 md:pb-0 marquee">
 @endif
    <i class="fas fa-rss fa-lg pr-4" style="color:yellow;"></i> {!! __("<b>Le saviez-vous :</b> Les <img src='img/gem10.png' class='w-5 h-5 inline-block'> vous permettent de jouer plus de parties, vous pouvez en acheter via le lien Pack de notre menu.") !!}</p>
 
