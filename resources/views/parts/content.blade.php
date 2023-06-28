6201e50e2d5b4c74e31e1c7dc1c23a3bcfdb28ee
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-28 23:05:38 +0200

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -105,9 +105,16 @@
                         <td class="pr-2">
                             <i class="fas fa-2x fa-user-group text-white"></i>
                         </td>
+                        @php
+                         $count = \App\Models\User::where('parrain', $lejoueur)->count();
+                        @endphp
+                        @if($count > 2)
+                            <td style='display:inline-block;' class='pl-2 text-white'>Limite de 3 parrainages atteinte</td>
+                        @else
                         <td style="display:inline-block;" class="pl-2 text-white">{{__('Remportez')}} 20 <img src='img/gem10.png'  style='display:inline-block;' class=' w-5 h-5 align-middle' alt='Gem 10'> {{__('par ami parrainé !')}}<br>
                         <b><a href="https://gokdo.com/admin/register?parrain={{ $lejoueur }}" data-barba-prevent="self" id="copyLink">{{__('Cliquez-ici pour copier votre lien')}}</a></b><br>
                         <i style="color: orange; font-size: 13px;">{{__("Toute triche sera synonyme d'exclusion du site")}}</i></td>
+                        @endif
                     </tr>
                 </tbody>
             </table>
