6f9fbfbb34ee8a463933cab3d90b473818569622
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-29 22:36:16 +0200

    ok

--- app/Http/Middleware/Localization.php
+++ app/Http/Middleware/Localization.php
@@ -5,7 +5,8 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Session;
+use App\Models\Session as AppSession; // Rename the conflicting Session class
+use Illuminate\Support\Facades\Session; // Use full namespace for Session facade
 use Symfony\Component\HttpFoundation\AcceptHeader;
 
 class Localization
@@ -20,37 +21,28 @@ class Localization
     public function handle(Request $request, Closure $next)
     {
         $availableLocales = config('app.available_locales');
-        $fallbackLocale = config('app.fallback_locale');
-
-        // Vérifier si la langue est déjà définie dans l'URL
-        if ($request->segment(1) && in_array($request->segment(1), $availableLocales)) {
-            $locale = $request->segment(1);
+        
+        // Vérifier si la langue est déjà définie dans la session
+        if (Session::has('locale') && in_array(Session::get('locale'), $availableLocales)) {
+            App::setLocale(Session::get('locale'));
         } else {
             // Récupérer la langue préférée du navigateur
             $acceptHeader = AcceptHeader::fromString($request->server->get('HTTP_ACCEPT_LANGUAGE'));
             $browserLocales = $acceptHeader->get('language');
-
+    
             // Utiliser la langue par défaut de l'application si aucune langue du navigateur n'est disponible
             if (!empty($browserLocales)) {
                 $preferredLanguage = $browserLocales[0]->getValue();
                 if (in_array($preferredLanguage, $availableLocales)) {
-                    $locale = $preferredLanguage;
+                    App::setLocale($preferredLanguage);
                 } else {
-                    $locale = $fallbackLocale;
+                    App::setLocale(config('app.fallback_locale'));
                 }
             } else {
-                $locale = $fallbackLocale;
+                App::setLocale(config('app.fallback_locale'));
             }
-        }
-
-        App::setLocale($locale);
-        Session::put('locale', $locale);
-
-        // Supprimer le segment de langue de l'URL
-        if ($request->segment(1) === $locale) {
-            $request->server->set('REQUEST_URI', substr($request->server->get('REQUEST_URI'), strlen($locale) + 1));
-        }
-
+        } 
+        
         return $next($request);
     }
 }
