da52331dbde251d9db4a50e31c55411b48ce503b
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-06-30 00:01:03 +0200

    ok

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,98 +1,89 @@
- @php
+@php
     use \App\Http\Controllers\GlobalController;
     $isMobile = GlobalController::isMobile();
 @endphp
- <!-- FOOTER -->
- <container id="footer" class="mx-auto text-gray-400 body-font max-w-7xl">
-     <div
-         class="container flex flex-col flex-wrap px-5 py-12 mx-auto md:items-center lg:items-start md:flex-row md:flex-nowrap">
-         <div class="flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left">
-             <a href="/" class="flex items-center justify-center font-medium text-gray-700 title-font md:justify-start">
 
-                 <h2 class="mb-3 text-sm font-medium tracking-widest text-blue-400 title-font">{{__('Jeux en ligne Multijoueurs')}}</h2>
-             </a>
-             <p class="mt-2 text-sm text-gray-400">{{__("Gagnez des cadeaux en jouant !")}}<br>
-                 {{__("GoKDO est un site de jeux gratuits sans obligation d'achat.")}}
-             </p>
-         </div>
-         <div class="flex flex-wrap flex-grow mt-10 -mb-10 text-center md:pl-20 md:mt-0 md:text-left">
-             <div class="w-full px-4 lg:w-1/4 md:w-1/2">
-             </div>
-             <div class="w-full px-4 lg:w-1/4 md:w-1/2">
-             </div>
-             <div class="w-full px-4 lg:w-1/4 md:w-1/2">
-                 <h2 class="mb-3 text-sm font-medium tracking-widest text-blue-400 title-font">{{__('A Propos')}}</h2>
-                 <div class="mb-10 list-none">
-                 <ul>
-                     <li>
-                         <a href="reglement" class="text-gray-400 hover:text-gray-500">{{__('Règlement')}}</a>
-                     </li>
-                     <li>
-                         <a href="mentions-legales" class="text-gray-400 hover:text-gray-500">{{__('Mentions Légales')}}</a>
-                     </li>
-                     <li>
-                         <a href="confidentialite-site" class="text-gray-400 hover:text-gray-500">{{__('Confidentialité')}}</a>
-                     </li>
-                  </ul>   
-                 </div>
-             </div>
-             <div class="w-full px-4 lg:w-1/4 md:w-1/2">
-                 <h2 class="mb-3 text-sm font-medium tracking-widest text-blue-400 title-font">{{__('Plus')}}</h2>
-                 <div class="mb-10 list-none">
-                 <ul>
-                     <li>
-                         <a href="aide" class="text-gray-400 hover:text-gray-500" data-barba-prevent="self">{{__('Aide')}}</a>
-                     </li>
-                     <li>
-                         <a href="contact" class="text-gray-400 hover:text-gray-500" data-barba-prevent="self">{{__('Nous Contacter')}}</a>
-                     </li>
-                </ul>
-                 </div>
-             </div>
-         </div>
-     </div>
-     <div class="bg-gray-700">
-         <div class="container flex flex-col flex-wrap px-5 py-4 mx-auto sm:flex-row">
-             <p class="text-sm text-center text-white sm:text-left">© 2023 GoKDO - {{__('Tous Droits Réservés')}}
-             </p>
-             <span class="inline-flex justify-center mt-2 sm:ml-auto sm:mt-0 sm:justify-start">
-                 <a href="https://www.facebook.com/gokdocom" class="text-white">
-                     <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                         class="w-5 h-5" viewBox="0 0 24 24">
-                         <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-                     </svg>
+<!-- FOOTER -->
+<container id="footer" class="mx-auto text-gray-400 body-font max-w-7xl">
+    <div class="container flex flex-col flex-wrap px-5 py-12 mx-auto md:items-center lg:items-start md:flex-row md:flex-nowrap">
+        <div class="flex-shrink-0 w-64 mx-auto text-center md:mx-0 md:text-left">
+            <a href="/" class="flex items-center justify-center font-medium text-gray-700 title-font md:justify-start">
+                <h2 class="mb-3 text-sm font-medium tracking-widest text-blue-400 title-font">{{__('Jeux en ligne Multijoueurs')}}</h2>
+            </a>
+            <p class="mt-2 text-sm text-gray-400">{{__("Gagnez des cadeaux en jouant !")}}<br>
+                {{__("GoKDO est un site de jeux gratuits sans obligation d'achat.")}}
+            </p>
+        </div>
+        <div class="flex flex-wrap flex-grow mt-10 -mb-10 text-center md:pl-20 md:mt-0 md:text-left">
+            <div class="w-full px-4 lg:w-1/4 md:w-1/2">
+            </div>
+            <div class="w-full px-4 lg:w-1/4 md:w-1/2">
+            </div>
+            <div class="w-full px-4 lg:w-1/4 md:w-1/2">
+                <h2 class="mb-3 text-sm font-medium tracking-widest text-blue-400 title-font">{{__('A Propos')}}</h2>
+                <div class="mb-10 list-none">
+                    <ul>
+                        <li>
+                            <a href="reglement" class="text-gray-400 hover:text-gray-500">{{__('Règlement')}}</a>
+                        </li>
+                        <li>
+                            <a href="mentions-legales" class="text-gray-400 hover:text-gray-500">{{__('Mentions Légales')}}</a>
+                        </li>
+                        <li>
+                            <a href="confidentialite-site" class="text-gray-400 hover:text-gray-500">{{__('Confidentialité')}}</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="w-full px-4 lg:w-1/4 md:w-1/2">
+                <h2 class="mb-3 text-sm font-medium tracking-widest text-blue-400 title-font">{{__('Plus')}}</h2>
+                <div class="mb-10 list-none">
+                    <ul>
+                        <li>
+                            <a href="aide" class="text-gray-400 hover:text-gray-500" data-barba-prevent="self">{{__('Aide')}}</a>
+                        </li>
+                        <li>
+                            <a href="contact" class="text-gray-400 hover:text-gray-500" data-barba-prevent="self">{{__('Nous Contacter')}}</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="bg-gray-700">
+        <div class="container flex flex-col flex-wrap px-5 py-4 mx-auto sm:flex-row">
+            <p class="text-sm text-center text-white sm:text-left">© 2023 GoKDO - {{__('Tous Droits Réservés')}}</p>
+            <span class="inline-flex justify-center mt-2 sm:ml-auto sm:mt-0 sm:justify-start">
+                <a href="https://www.facebook.com/gokdocom" class="text-white">
+                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        class="w-5 h-5" viewBox="0 0 24 24">
+                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                    </svg>
                     @if($isMobile == true)
                     Facebook
-                    @else
                     @endif
-
-                 </a>
-                 <a href="https://www.instagram.com/gokdocom/" class="ml-3 text-white">
-                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                         stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                         <rect width="20" height="20" x="2" y="2" rx="5"
-                             ry="5"></rect>
-                         <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
-                     </svg>
+                </a>
+                <a href="https://www.instagram.com/gokdocom/" class="ml-3 text-white">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                        stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                        <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                        <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                    </svg>
                     @if($isMobile == true)
                     Instagram
-                    @else
                     @endif
-
-                 </a>
-                 <a href="https://twitter.com/gokdocom" class="ml-3 text-white">
-                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5"
-                        viewBox="0 0 24 24">
+                </a>
+                <a href="https://twitter.com/gokdocom" class="ml-3 text-white">
+                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        class="w-5 h-5" viewBox="0 0 24 24">
                         <path
                             d="M23 3.5C22.042 4.157 20.982 4.65 19.86 4.956C19.32 4.648 18.726 4.47 18.107 4.43C18.745 4.096 19.256 3.56 19.543 2.91C18.525 3.311 17.46 3.59 16.4 3.75C15.423 2.773 14.078 2.25 12.62 2.25C10.82 2.25 9.338 3.733 9.338 5.55C9.338 5.831 9.369 6.104 9.43 6.366C6.227 6.214 3.553 4.941 1.445 2.812C1.093 3.387 0.883 4.065 0.883 4.79C0.883 6.113 1.625 7.27 2.757 7.786C2.198 7.767 1.666 7.622 1.196 7.366C1.195 7.381 1.195 7.396 1.195 7.412C1.195 9.398 2.66 10.932 4.641 11.207C4.33 11.293 3.999 11.337 3.66 11.337C3.428 11.337 3.198 11.318 2.97 11.282C3.437 12.643 4.63 13.625 6.119 13.654C4.708 14.78 2.862 15.416 0.867 15.416C0.578 15.416 0.293 15.399 0.01 15.364C1.515 16.508 3.412 17.187 5.47 17.187C12.624 17.187 16.153 11.18 16.153 6.482C16.153 6.34 16.15 6.199 16.143 6.058C17.253 5.3 18.174 4.298 18.825 3.07C18.527 3.11 18.221 3.134 17.912 3.134C17.694 3.134 17.48 3.118 17.27 3.086C17.702 3.936 17.965 4.912 17.965 5.96V6.323C18.67 6.122 19.347 5.798 19.956 5.368C19.416 5.59 18.835 5.734 18.233 5.794C18.71 5.47 19.109 5.026 19.378 4.497C18.793 4.768 18.158 4.961 17.507 5.07C17.974 4.748 18.402 4.322 18.757 3.812Z" />
                     </svg>
                     @if($isMobile == true)
                     Twitter
-                    @else
                     @endif
                 </a>
-
-             </span>
-         </div>
-     </div>
- </container>
+            </span>
+        </div>
+    </div>
+</container>
