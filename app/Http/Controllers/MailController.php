a9df7de5c48866c4244504e7747550bb929cd87b
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-07-02 22:55:20 +0200

    ok

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+  
 use Illuminate\Http\Request;
 use App\Mail\MyMail;
 use App\Models\Settings;
@@ -16,24 +16,27 @@ class MailController extends Controller
      *
      * @return response()
      */
-    public function sendMessage(Request $request)
-    {
+    public function sendMessage (Request $request) {
         $usermail = env('MAIL_USERNAME');
-
+       
         $request->validate([
             'email' => 'required|email',
             'name' => 'required|string',
             'g-recaptcha-response' => 'required|recaptchav3:register,0.5'
         ]);
 
-        if ($request->session()->exists('mail')) {
-            return back()->with('already_send', 'ok');
-        } else {
-            Mail::to($request->input('email'))->send(new MyMail($request->all()));
 
-            $request->session()->put('mail', '1');
-
-            return back()->with('Mail_envoye', 'ok');
+       if ($request->session()->exists('mail')) {
+             return back()->with('already_send', 'ok');
         }
+       else 
+       {
+        Mail::to($usermail)->queue(new MyMail($request->all()));
+
+        $request->session()->put('mail', '1');
+
+        return back()->with('Mail_envoye', 'ok');
+       }
+       
     }
 }
