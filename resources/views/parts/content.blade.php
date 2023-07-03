a3d7819a7e0099f94790672952f178f4c322c7de
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-07-03 13:56:17 +0200

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -541,7 +541,7 @@
                                 @else
                                 @endif
                                 @endif
-                                    <a href="{{ $locale }}/game/{{ $sologame->id }}" onclick="event.preventDefault(); window.location.reload(true); window.location.href='game?id={{ $sologame->id }}';"
+                                    <a href="{{ $locale }}/game/{{ $sologame->id }}" onclick="event.preventDefault(); window.location.reload(true); window.location.href='{{ $locale }}/game/{{ $sologame->id }}';"
                                         class="relative flex justify-center w-24 px-5 py-2 mx-auto mt-4 font-medium text-white shadow-lg group">
                                         <span
                                             class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-indigo-500 group-hover:bg-indigo-700 group-hover:skew-x-12"></span>
