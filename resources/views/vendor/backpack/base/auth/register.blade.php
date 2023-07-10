61bed474a92edf6d70f5308a8533bcc6e36724b8
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-07-10 17:00:22 +0200

    ok

--- resources/views/vendor/backpack/base/auth/register.blade.php
+++ resources/views/vendor/backpack/base/auth/register.blade.php
@@ -171,7 +171,7 @@ switch($locale){case'en':$rules="By signing up, you accept the";$rules2="rules";
             @endif
             <div class="text-center"><a href="{{ route('backpack.auth.login') }}">{{ trans('backpack::base.login') }}</a> /
             <a href="{{ route('backpack.auth.password.reset') }}">{{ trans('backpack::base.forgot_your_password') }}</a></div>
-               <div class="text-center"><br>{{ $rules }} <a href='reglement'>{{ $rules2 }}</a>
+               <div class="text-center"><br>{{ $rules }} <a href="{{ str_replace('/admin/login', '/reglement', route('backpack.auth.login')) }}">{{ $rules2 }}</a>
                 </div></div>
             </div>
           
