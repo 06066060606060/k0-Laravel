acf69c9b219934e1c4972e65ffa16f939d81a9dc
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-07-10 23:41:46 +0200

    ij

--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -22,6 +22,11 @@
 
 <meta name="coverage" content="Worldwide" />
 <link rel="canonical" href="https://gokdo.com" />
+<link rel="canonical" href="https://en.gokdo.com" />
+<link rel="canonical" href="https://it.gokdo.com" />
+<link rel="canonical" href="https://es.gokdo.com" />
+<link rel="canonical" href="https://fr.gokdo.com" />
+<link rel="canonical" href="https://de.gokdo.com" />
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
 
 <script src="https://kit.fontawesome.com/59ecaaffaa.js" crossorigin="anonymous" async></script>
