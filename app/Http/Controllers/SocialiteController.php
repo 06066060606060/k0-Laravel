67ab587aa6273758c8c3ca3a5da5a5cdb5637318
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-07-19 03:57:48 +0200

    ok

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -26,7 +26,7 @@ class SocialiteController extends Controller
 
     // On vérifie si le provider est autorisé
     if (in_array($provider, $this->providers)) {
-        $redirectUri = url('callback/' . $provider);
+        $redirectUri = url('/callback/' . $provider);
         return Socialite::driver($provider)
             ->with(['redirect_uri' => $redirectUri])
             ->redirect(); // On redirige vers le provider
