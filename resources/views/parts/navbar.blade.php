f78b8f84a1a55feb0b3ab28997854cc3bdc4fff9
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-07-20 13:15:03 +0200

    ok

--- resources/views/parts/navbar.blade.php
+++ resources/views/parts/navbar.blade.php
@@ -88,7 +88,7 @@ $isMobile = GlobalController::isMobile();
                         </a>
                         @endif
                     
-                    <a class="text-sm font-bold text-gray-400  lg:mx-4 hover:text-blue-600 lg:pr-6" href="vip"
+                    <a class="text-sm font-bold text-gray-400  lg:mx-4 hover:text-blue-600 lg:pr-6" style="color:#e920e0;" href="vip"
                         @click="isOpen = false"  data-barba-prevent="self">
                         <i class="fa-solid fa-rocket"></i>&nbsp; {{__('VIP')}}
                     </a>
