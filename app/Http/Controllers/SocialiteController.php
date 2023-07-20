a455b72e349e50538d1de10113c0e0c1c5319f40
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-07-20 10:31:02 +0200

    ok

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -134,7 +134,8 @@ public function callback(Request $request)
 
         # 3. On connecte l'utilisateur
         backpack_auth()->login($user);
-        
+        backpack_auth()->login($user);
+
         # 4. On redirige l'utilisateur vers /home avec un message de succès
         return redirect('/')->with('success', 'Vous êtes maintenant connecté.');
     }
