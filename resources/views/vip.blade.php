8f3ab20fdafdadfe7889057b64b89b0d0fddc861
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-07-21 00:30:12 +0200

    ok

--- resources/views/vip.blade.php
+++ resources/views/vip.blade.php
@@ -18,34 +18,34 @@
 <table class="border-white w-full text-center rounded-lg">
   <tr>
     <th width="50%" style="border-radius:0.5rem 0 0 0;" class="border-white py-4 bg-gray-700 text-white">VIP MINI</th>
-    <th width="50%" style="border-radius:0 0.5rem 0 0;" class="border-white py-4 bg-gray-700 text-white">GIFT</th>
+    <th width="50%" style="border-radius:0 0.5rem 0 0;" class="border-white py-4 bg-gray-700 text-white">{{__("CADEAU")}}</th>
   </tr>
   <tr>
     <td class="border-white py-4 text-sm font-bold bg-white text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">1 Pack Mini acheté</td>
+">1 {{__("Pack Mini acheté")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">20 000 <img src="/img/diamond5.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 text-sm font-bold bg-white text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">10 Packs Mini achetés</td>
+">10 {{__("Packs Mini achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">200 000 <img src="/img/diamond5.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">50 Packs Mini achetés</td>
+">50 {{__("Packs Mini achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">100 <img src="/img/gem10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">100 Packs Mini achetés</td>
+">100 {{__("Packs Mini achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">200 <img src="/img/gem10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
-    <td class="border-white py-4 bg-white text-sm font-bold text-black">500 Packs Mini achetés</td>
+    <td class="border-white py-4 bg-white text-sm font-bold text-black">500 {{__("Packs Mini achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black"><span class="inline-flex">100 <img src="/img/coin10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
 </table>
@@ -54,34 +54,34 @@
 <table class="border-white w-full text-center">
   <tr>
     <th width="50%" class="border-white py-4 bg-gray-700 text-white">VIP STARTER</th>
-    <th width="50%" class="border-white py-4 bg-gray-700 text-white">GIFT</th>
+    <th width="50%" class="border-white py-4 bg-gray-700 text-white">{{__("CADEAU")}}</th>
   </tr>
   <tr>
     <td class="border-white py-4 text-sm font-bold bg-white text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">1 Pack Starter acheté</td>
+">1 {{__("Pack Starter acheté")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">50 000 <img src="/img/diamond5.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 text-sm font-bold bg-white text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">10 Packs Starter achetés</td>
+">10 {{__("Packs Starter achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">500 000 <img src="/img/diamond5.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">50 Packs Starter achetés</td>
+">50 {{__("Packs Starter achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">250 <img src="/img/gem10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">100 Packs Starter achetés</td>
+">100 {{__("Packs Starter achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">500 <img src="/img/gem10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
-    <td class="border-white py-4 bg-white text-sm font-bold text-black">500 Packs Starter achetés</td>
+    <td class="border-white py-4 bg-white text-sm font-bold text-black">500 {{__("Packs Starter achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black"><span class="inline-flex">250 <img src="/img/coin10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
 </table>
@@ -90,34 +90,34 @@
 <table class="border-white w-full text-center rounded-lg">
   <tr>
     <th width="50%" class="border-white py-4 bg-gray-700 text-white">VIP BOOSTER</th>
-    <th width="50%" class="border-white py-4 bg-gray-700 text-white">GIFT</th>
+    <th width="50%" class="border-white py-4 bg-gray-700 text-white">{{__("CADEAU")}}</th>
   </tr>
   <tr>
     <td class="border-white py-4 text-sm font-bold bg-white text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">1 Packs Booster achetés</td>
+">1 {{__("Pack Booster acheté")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">100 000 <img src="/img/diamond5.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 text-sm font-bold bg-white text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">10 Packs Booster achetés</td>
+">10 {{__("Packs Booster achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">1 000 000 <img src="/img/diamond5.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">50 Packs Booster achetés</td>
+">50 {{__("Packs Booster achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">500 <img src="/img/gem10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">100 Packs Booster achetés</td>
+">100 {{__("Packs Booster achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">1 000 <img src="/img/gem10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
-    <td class="border-white py-4 bg-white text-sm font-bold text-black">500 Packs Booster achetés</td>
+    <td class="border-white py-4 bg-white text-sm font-bold text-black">500 {{__("Packs Booster achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black"><span class="inline-flex">500 <img src="/img/coin10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
 </table>
@@ -126,34 +126,34 @@
 <table class="border-white w-full text-center rounded-lg">
   <tr>
     <th width="50%" class="border-white py-4 bg-gray-700 text-white">VIP MAXI</th>
-    <th width="50%" class="border-white py-4 bg-gray-700 text-white">GIFT</th>
+    <th width="50%" class="border-white py-4 bg-gray-700 text-white">{{__("CADEAU")}}</th>
   </tr>
   <tr>
     <td class="border-white py-4 text-sm font-bold bg-white text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">1 Packs Maxi achetés</td>
+">1 {{__("Pack Maxi acheté")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">200 000 <img src="/img/diamond5.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 text-sm font-bold bg-white text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">10 Packs Maxi achetés</td>
+">10 {{__("Packs Maxi achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">2 000 000 <img src="/img/diamond5.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">50 Packs Maxi achetés</td>
+">50 {{__("Packs Maxi achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">1 000 <img src="/img/gem10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">100 Packs Maxi achetés</td>
+">100 {{__("Packs Maxi achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">2 000 <img src="/img/gem10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
-    <td class="border-white py-4 bg-white text-sm font-bold text-black">500 Packs Maxi achetés</td>
+    <td class="border-white py-4 bg-white text-sm font-bold text-black">500 {{__("Packs Maxi achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black"><span class="inline-flex">1 000 <img src="/img/coin10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
 </table>
@@ -162,34 +162,34 @@
 <table class="border-white w-full text-center rounded-lg">
   <tr>
     <th width="50%" class="border-white py-4 bg-gray-700 text-white">VIP TERA</th>
-    <th width="50%" class="border-white py-4 bg-gray-700 text-white">GIFT</th>
+    <th width="50%" class="border-white py-4 bg-gray-700 text-white">{{__("CADEAU")}}</th>
   </tr>
   <tr>
     <td class="border-white py-4 text-sm font-bold bg-white text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">1 Packs Tera achetés</td>
+">1 {{__("Pack Tera acheté")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">500 000 <img src="/img/diamond5.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 text-sm font-bold bg-white text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">10 Packs Tera achetés</td>
+">10 {{__("Packs Tera achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">5 000 000 <img src="/img/diamond5.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">50 Packs Tera achetés</td>
+">50 {{__("Packs Tera achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">2 500 <img src="/img/gem10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">100 Packs Tera achetés</td>
+">100 {{__("Packs Tera achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">5 000 <img src="/img/gem10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
-    <td class="border-white py-4 bg-white text-sm font-bold text-black">500 Packs Tera achetés</td>
+    <td class="border-white py-4 bg-white text-sm font-bold text-black">500 {{__("Packs Tera achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black"><span class="inline-flex">2 500 <img src="/img/coin10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
 </table>
@@ -198,35 +198,35 @@
 <table class="border-white w-full text-center rounded-lg">
   <tr>
     <th width="50%" class="border-white py-4 bg-gray-700 text-white">VIP EXPERT</th>
-    <th width="50%" class="border-white py-4 bg-gray-700 text-white">GIFT</th>
+    <th width="50%" class="border-white py-4 bg-gray-700 text-white">{{__("CADEAU")}}</th>
   </tr>
   <tr>
     <td class="border-white py-4 text-sm font-bold bg-white text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">1 Pack Expert acheté</td>
+">1 {{__("Pack Expert acheté")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">1 000 000 <img src="/img/diamond5.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 text-sm font-bold bg-white text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">10 Packs Expert achetés</td>
+">10 {{__("Packs Expert achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">10 000 000 <img src="/img/diamond5.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">50 Packs Expert achetés</td>
+">50 {{__("Packs Expert achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">5 000 <img src="/img/gem10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">100 Packs Expert achetés</td>
+">100 {{__("Packs Expert achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">10 000 <img src="/img/gem10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
   <tr>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-radius:0 0 0 0.5rem; border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
-">500 Packs Expert achetés</td>
+">500 {{__("Packs Expert achetés")}}</td>
     <td class="border-white py-4 bg-white text-sm font-bold text-black" style="border-radius:0 0 0.5rem 0; border-bottom: 1px solid rgb(55 65 81 / var(--tw-bg-opacity));
 "><span class="inline-flex">5 000 <img src="/img/coin10.png" class="ml-2 mt-0 w-7 h-6"></span></td>
   </tr>
