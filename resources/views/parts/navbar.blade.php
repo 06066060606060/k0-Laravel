eae73007055eda2f6bdfd3b1cbe38217dcd261d6
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-07-20 13:11:34 +0200

    ok

--- resources/views/parts/navbar.blade.php
+++ resources/views/parts/navbar.blade.php
@@ -92,6 +92,10 @@ $isMobile = GlobalController::isMobile();
                         @click="isOpen = false"  data-barba-prevent="self">
                         <i class="fa-solid fa-user"></i>&nbsp; {{__('Profil')}}
                     </a>
+                    <a class="text-sm font-bold text-gray-400  lg:mx-4 hover:text-blue-600 lg:pr-6" href="vip"
+                        @click="isOpen = false"  data-barba-prevent="self">
+                        <i class="fa-solid fa-rocket"></i>&nbsp; {{__('VIP')}}
+                    </a>
                 @endif
                 <a class="text-sm font-bold text-gray-400  lg:mx-4 hover:text-blue-600 lg:pr-6" href="#"
                     @click="isOpen = false"  data-barba-prevent="self">
