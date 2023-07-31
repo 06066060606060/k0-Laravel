5258d966065dc21588c085e895cc4114b267ec3b
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-07-31 11:27:43 +0200

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -839,7 +839,7 @@ $isLanguageSubdomain = in_array($languageSubdomain, $languages); ?>
                     <div
                         class="flex flex-col items-start mt-12 mb-16 text-left lg:flex-grow lg:w-1/2 lg:pl-6 xl:pl-24 md:mb-0 xl:mt-0">
                     <span class="mb-4 font-bold tracking-widest text-blue-600 uppercase text-md">
-                        @if($starred->name == 'GoFRUITS' || $starred-> == 'Egypt')
+                        @if($starred->name == 'GoFRUITS' || $starred->name == 'Egypt')
                             {{__('JOUEZ GRATUITEMENT A')}}
                         @elseif($starred->name == 'Jungle')
                             {{__('Jeux de Grattage')}}
