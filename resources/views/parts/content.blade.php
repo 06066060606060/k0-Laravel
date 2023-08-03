9074a9fa5594cd9c337e8fef76f5611921cdeccd
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-08-03 15:27:26 +0200

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -765,10 +765,8 @@ $isLanguageSubdomain = in_array($languageSubdomain, $languages); ?>
                                                 @else
                                                     <h2 class="pb-0 pl-4 font-semibold text-s">{{ $score->name }}</h2>
                                                 @endif
-                                                @if($amount > 0)
                                                     <span href="#"
                                                           class="ml-4 text-m font-bold text-blue-700 lg:mb-0">{{ $cadeau_name }}</span>
-                                                @endif
                                                 @if($isMobile)
                                                     <span href="#"
                                                           class="ml-4 text-xs font-bold text-orange-600 lg:mb-0">{{ $gameName }}</span>
