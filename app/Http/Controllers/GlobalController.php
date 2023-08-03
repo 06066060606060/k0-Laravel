ee44bd54c6b723b6595a9627a616f3a5275c2ad4
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-08-03 15:43:49 +0200

    ok

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -59,8 +59,10 @@ class GlobalController extends Controller
         $scores = Commandes::select('commandes.*', 'users.name', 'cadeaux.name as cadeau_name', 'cadeaux.name_de as cadeau_name_de', 'cadeaux.name_en as cadeau_name_en', 'cadeaux.name_es as cadeau_name_es', 'cadeaux.name_it as cadeau_name_it')
         ->join('users', 'users.id', '=', 'commandes.user_id')
         ->leftJoin('cadeaux', 'cadeaux.id', '=', 'commandes.cadeau_id')
+        ->latest() // Trie les résultats par date de création, en ordre décroissant (les derniers en premier)
         ->take(10)
         ->get();
+    
             
         // Tous les jeux
         $allgames = Games::whereNotIn('type', ['Event', 'Solo', 'Grattage'])->orderBy('id', 'desc')->get();
