411b036fe0231178babca6bcb5c83988853f1885
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-08-08 19:16:41 +0200

    ok

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -80,6 +80,7 @@
         // Vérifier si les dimensions correspondent à un téléphone portable
         if (screenWidth <= 480 && screenHeight <= 800) {
             document.getElementById("gameBody").classList.remove("h-[667px]");
+            document.getElementById("gameBody").classList.add("mb-4 py-4");
         } else {
             document.getElementById("gameBody").classList.add("h-[667px] mb-4");
         }
