a151ecefbaa216d9637947907733ba8dcac9310c
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-08-08 22:01:58 +0200

    ok

---
+++ database/migrations/2023_08_08_200132_add_translation_columns_to_packs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('packs', function (Blueprint $table) {
+            $table->string('add')->nullable();
+            $table->string('add_en')->nullable();
+            $table->string('add_de')->nullable();
+            $table->string('add_es')->nullable();
+            $table->string('add_it')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('packs', function (Blueprint $table) {
+            //
+        });
+    }
+};
