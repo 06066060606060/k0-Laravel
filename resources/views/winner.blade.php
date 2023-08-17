41b39f413d59426014a819b8824b402ea9fdd65a
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-08-17 19:03:15 +0200

    ok

--- resources/views/winner.blade.php
+++ resources/views/winner.blade.php
@@ -48,7 +48,7 @@
                     @else
                     @endif    
                     <div class="">
-                            <a href="game/{{ $concours->game_id }}"
+                            <a href="game/{{ $concours->game_id }}" data-barba-prevent="self"
                                 class="relative px-5 py-2 mx-auto mt-4 font-medium text-white shadow-lg group">
                                 <span
                                     class="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-indigo-500 group-hover:bg-indigo-700 group-hover:skew-x-12"></span>
