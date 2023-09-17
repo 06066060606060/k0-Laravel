868d2dd51c7a4c2f7938ed437f2b83361ce879bb
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-09-17 23:25:50 +0200

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -1138,6 +1138,8 @@ prevBtn.addEventListener("click", function() {
                             {{__('JEUX DE GRATTAGE')}}
                         @elseif($starred->name == 'Tresor')
                             {{__('JEUX DE CHASSE AU TRESOR')}}
+                        @elseif($starred->name == 'Stone')
+                            {{__('JEUX DE GRILLE')}}
                         @else
                             {{__('JEUX MULTIJOUEURS')}}
                         @endif
