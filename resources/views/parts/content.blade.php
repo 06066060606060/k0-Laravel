bae6632976ff770c7b6b974fa4f0d3dc51b26324
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-09-18 20:33:42 +0200

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -995,7 +995,7 @@
                     <div
                         class="flex flex-col items-start 
                         @if($isMobile == true) 
-                        mt-2 mb-6
+                        mt-2 mb-8
                         @else 
                         mt-12 mb-16
                         @endif        
