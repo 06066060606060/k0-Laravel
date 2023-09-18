a6238800a04d080242d45beb4cf3775959fde51a
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-09-18 22:55:20 +0200

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -974,7 +974,7 @@
                                 @endphp
                                 <a href="admin/register">
                                     <img class="object-cover object-center mx-auto rounded-lg shadow-2xl" alt="hero"
-                                         src="{{ $imgiUrls }}" width="920" height="420"
+                                         src="{{ $imgiUrls }}"
                                          onerror="this.src='/img/empty.png'">
                                 </a>
                             </div>
