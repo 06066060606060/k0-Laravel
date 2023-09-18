2b2eaab19bc452ae69fe0374ba4a3495e173bd03
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-09-18 12:37:27 +0200

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,10 @@
     @include('parts.head')
 </head>
 <body class="overflow-x-hidden bg-gray-900 pattern" data-barba="wrapper">
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TLHCZTD3"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <navbar>
         @include('parts.navbar')
     </navbar>
--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -56,6 +56,14 @@
 </script>
 <!-- End of Taboola Pixel Code -->
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TLHCZTD3');</script>
+<!-- End Google Tag Manager -->
+
 <script src="https://kit.fontawesome.com/59ecaaffaa.js" crossorigin="anonymous" async></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" async></script>
 
