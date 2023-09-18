d71ffd1b2bf886736137ca468e9881f9b2cb1cd5
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-09-18 14:14:12 +0200

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -97,7 +97,10 @@
              @php
                 $winnersText = "";
                 foreach ($scores as $score) {
-                    $winnersText .= '<img alt="" class="inline-block object-center w-auto h-' . ($isMobile ? '9' : '12') . '" src="https://i.pinimg.com/originals/5c/15/c1/5c15c1539c9c566b5413d98f9cf3592f.png"> ' . $score->name . ' gagne ' . $score->cadeau_name . '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;   ';
+                    $winnersText .= '                                    <div
+                                        class="flex flex-col w-full max-w-md p-8 mx-4 text-left bg-white shadow-lg rounded-xl h-28">
+                                        <div class="flex">
+<img alt="" class="inline-block object-center w-auto h-' . ($isMobile ? '9' : '12') . '" src="https://i.pinimg.com/originals/5c/15/c1/5c15c1539c9c566b5413d98f9cf3592f.png"> ' . $score->name . ' gagne ' . $score->cadeau_name . '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div></div>   ';
                 }
             @endphp
             {!! $winnersText !!} <
