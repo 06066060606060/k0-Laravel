6329ca33aba1c83c4a5437d6aa76ebaf47dd5bbe
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-09-18 21:40:26 +0200

    ok

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -92,7 +92,7 @@
             </div>
             <div class="flex flex-col w-full py-2">
 
-                <h1 class="pt-0 text-lg font-bold text-white">{{__('Mes recharges')}}:</h1>
+                <h1 class="pt-0 text-lg font-bold text-white">{{__('Mes Packs achetés')}}:</h1>
                 <div class="flex flex-col w-full mt-4 mb-4 bg-gray-800 border border-gray-700 rounded-xl md:mb-0 max-h-64">
                     <div class="overflow-x-auto rounded-t-lg">
                         <table class="min-w-full py-2 text-sm divide-y divide-gray-200 ">
