86377733739987fae6774c1c9b8532bcd0f86588
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-09-23 16:35:12 +0200

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -114,7 +114,7 @@
                 class="mb-4 px-2 py-2 mx-8 bg-gray-800 rounded-lg lg:mx-8 xl:mx-auto bg-opacity-40 max-w-7xl sm:px-16 md:px-24 lg:py-18">
                 <h2 class="text-2xl font-bold tracking-tight text-center text-gray-100 ">
 Gagnez des Diamants FACILEMENT avec les sondages !</h2>
-                <h2 class="text-xs font-bold tracking-tight text-center text-gray-100 ">
+                <h2 class="text-s tracking-tight text-center text-gray-100 ">
                 <i>Un sondage complété donne aussi 1 point de concours</i>
                 </h2>
 <iframe width="100%" frameBorder="0" height="500px"  src="https://offers.cpx-research.com/index.php?app_id=20132&ext_user_id={{ backpack_auth()->user()->id }}&secure_hash=0INpuQOyRvhHLJLMd9IPO57IDDbGBbZP&username={{ backpack_auth()->user()->name }}&email={{ backpack_auth()->user()->email }}&subid_1={{ backpack_auth()->user()->name }}&subid_2"></iframe>
