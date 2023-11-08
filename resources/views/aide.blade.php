068813e01e58a7c5c4dc0b5f15cd0e3b65673294
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-11-08 23:04:03 +0100

    ok

--- resources/views/aide.blade.php
+++ resources/views/aide.blade.php
@@ -25,25 +25,14 @@
                         <div class="px-6 py-6 text-white">{{__("Le Diamant est la principale monnaie virtuelle du site. Les Diamants se cumulent lorsque vous jouez sur les jeux et vous permettent de commander des Cadeaux. Vous pouvez obtenir des Diamants en jouant aux jeux GoFRUITS, POOL mais également via le concours et les Events. Les Diamants que vous gagnez sont stockés dans votre Compte Joueur. Pour consulter votre Compte Joueur, votre historique de gains et les Cadeaux que vous avez gagnés et commandés, il vous suffit de cliquer sur 'Profil' en haut à droite de la page. Il est important de noter que les Diamants n'ont aucune valeur monétaire.")}}</div>
                     </div>
                     </div>
-                    <div class="accordion-section bg-gray-800 bg-opacity-40 mb-4">
-                    <h2 class="accordion-title text-l">{{__("Qu'est-ce qu'un Rubis ?")}}</h2>
-                    <div class="accordion-content mb-2">
-                        <div class="px-6 py-6 text-white">{{__("Les Rubis vous permettent d'obtenir des parties Bonus sur le jeu GoFRUITS, de pouvoir jouer au jeu POOL ainsi que d'autres jeux de type Events et d'obtenir des objets virtuels dans le jeu Tresor. Vous pouvez obtenir des Rubis dans la section des Cadeaux en échange des Diamants de votre compte, ou dans l'espace 'Packs'.")}}</div>
-                    </div>
                     </div>
                     <div class="accordion-section bg-gray-800 bg-opacity-40 mb-8">
                     <h2 class="accordion-title text-l">{{__("Qu'est-ce qu'un Coins ?")}}</h2>
-                    <div class="accordion-content mb-2">
                         <div class="px-6 py-6 text-white">{{__("Le Coin est la monnaie secondaire du site, 1 Coin = 1€. Vous pouvez cumuler vos Coins en les gagnants sur le jeu POOL, le concours ou encore les jeux Events. Une boutique dédiée spécialement aux Coins est disponible dans la page 'Cadeaux' en cliquant sur l'onglet 'COINS', vous pourrez alors échanger contre des bons Amazon mais également des versements Paypal.")}}</div>
                     </div>
-                    </div>
-
-                    <h2 class="mb-1 text-2xl font-bold text-blue-500 title-font">{{__('Les jeux')}}</h2>
                     <div class="accordion-section bg-gray-800 bg-opacity-40 mb-8">
-                    <h2 class="accordion-title">{{__('Comment participer au concours ?')}}</h2>
-                    <div class="accordion-content mb-4">
-                    <div class="px-6 py-6 text-white">{{__("Lorsqu'un concours est en place, il vous suffit de vous rendre sur la page 'Concours' afin de voir quel jeu vous permettra de récolter des points de concours. Le concours est défini d'une date de départ et une date de fin, les 3000 premiers joueurs classés au concours seront récompensé lorsque le concours sera terminé. Les gains alloués au concours peuvent être des bons Amazon, des versements Paypal, des Coins, Rubis ou Diamants. Dans tous les cas le concours sera gratuit sans obligation d'achat.")}}</div>
-                    </div>
+                    <h2 class="accordion-title">{{__('Comment participer au concours parrainage ?')}}</h2>
+                    <div class="px-6 py-6 text-white">{{__("Pour participer au concours de parrainage, il vous suffit simplement de parrainer de nouveaux membres sur Gokdo, un membre parrainé vous rapportera 1 point de concours parrainage, plus vous parrainez et plus vous aurez de chance de remporter de gains !")}}</div>
                     </div>
 
                     
