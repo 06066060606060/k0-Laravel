06d8a602a4723a01407291905533aea9cfb5c635
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-11-10 00:41:22 +0100

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -293,12 +293,12 @@ Complétez des sondages rémunérés !</h2>
                         <div class="relative w-full max-w-lg">
                             <div class="relative">
                 @if($isMobile == true)
-<video width="100%" height="150" controls autoplay loop>
+<video width="100%" height="150" controls autoplay loop muted>
     <source src="/img/sondages.mp4" type="video/mp4">
     Votre navigateur ne prend pas en charge la balise vidéo.
 </video>
 @else
-<video width="560" height="315" controls autoplay loop>
+<video width="560" height="315" controls autoplay loop muted>
     <source src="/img/sondages.mp4" type="video/mp4">
     Votre navigateur ne prend pas en charge la balise vidéo.
 </video>
