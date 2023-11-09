e2962d1727fdeac347f505a6bfdd15e83d9a9fd4
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-11-10 00:07:50 +0100

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -294,7 +294,7 @@ Complétez des sondages rémunérés !</h2>
                             <div class="relative">
                 @if($isMobile == true)
                                 <a href="admin/register" style="display: block; width: 100%;">
-    <iframe width="290" height="150" src="https://www.youtube.com/embed/_ApmMSreGSw?si=swc8xWXeXRwczz7W" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <iframe width="100%" height="150" src="https://www.youtube.com/embed/_ApmMSreGSw?si=swc8xWXeXRwczz7W" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </a>@else
                            <a href="admin/register" style="display: block; width: 100%;">
     <iframe width="560" height="315" src="https://www.youtube.com/embed/_ApmMSreGSw?si=swc8xWXeXRwczz7W" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
