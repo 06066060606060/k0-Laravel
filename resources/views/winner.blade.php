e1e013103d876801227eee783f5b750c7fff9727
VIZIATI MICHEL <gokdo.com@gmail.com>
2023-11-09 23:53:27 +0100

    ok

--- resources/views/winner.blade.php
+++ resources/views/winner.blade.php
@@ -72,7 +72,7 @@
                                     class="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-indigo-600 -rotate-12"></span>
                                 <span
                                     class="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-indigo-400 -rotate-12"></span>
-                                <span class="relative">{{__('Cliquez-ici pour copier votre lien')}}</span></a></b><br>
+                                <span class="relative">{{__('Cliquez-ici pour copier votre lien')}}</span></a></b><br><br>
                                         <i style="color: orange; font-size: 13px;">{{__("Triche = Exclusion du site")}}</i>
                                     </td>
                                 </tr>
