35cb974e2e1aa434925d6b8395d516494320ae21
Gokdocom <gokdo.com@gmail.com>
2024-02-10 14:48:45 +0100

    ok

--- resources/views/parts/content.blade.php
+++ resources/views/parts/content.blade.php
@@ -404,9 +404,9 @@ Complétez des sondages rémunérés !</h2>
                         @if($isMobile == true)
                         @else
                         <p class="mb-4 text-base leading-relaxed text-left text-gray-300">
-                        Partagez votre avis et gagnez des récompenses, tel est le concept du site Gokdo.com.<br>
-                        Participez à des sondages en ligne, vous avez la chance de pouvoir d'influencer le développement des produits et services que vous consommerez demain. Exprimez-vous en participant à des sondages rémunérés, des tests de produits et des visites mystères en échange d'argent ou de cartes cadeaux.
-                        Participez aux sondages rémunérés pour gagner des récompenses, ce site reverse automatiquement 50% des bénéfices réalisés.<br><br> Parrainez de nouveaux membres pour gagner au concours de parrainage, 200€ mis en jeu chaque mois !</p>
+                        Partagez votre avis et gagnez des récompenses, tel est le concept du site Gokdo.com.<br><br>
+                        Participez à des sondages rémunérés en ligne, vous avez la chance de pouvoir influencer le développement des produits et services que vous consommerez demain. Exprimez-vous en participant au sondage rémunéré maintenant en échange de récompenses.
+                        <br><br>Ce site reverse automatiquement 50% des bénéfices réalisés.<br><br> Parrainez de nouveaux membres pour gagner au concours de parrainage, 200€ mis en jeu chaque mois !</p>
                         @endif
                         @if($isMobile == true)
                         <div class="mt-4">
