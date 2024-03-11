cbbcf20579a07fc0fcb5b3d03391db87ca6801f7
Gokdocom <gokdo.com@gmail.com>
2024-03-11 13:34:43 +0100

    ok

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -4,14 +4,14 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i>{{ trans('backpack::base.dashboard') }}</a></li>
 
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon la la-user"></i><span>Utilisateurs</span></a></li>
-
+<!--
 <li class="nav-item nav-dropdown">
     <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon las la-trophy"></i> Concours</a>
     <ul class="nav-dropdown-items">
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('concours') }}"><i class="nav-icon las la-trophy "></i> Concours</a>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('gains') }}"><i class="nav-icon la la-th-list"></i> Gains</a></li>
     </ul>
-</li>
+</li>-->
 
 <li class="nav-item nav-dropdown">
     <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon las la-store "></i> Boutique</a>
