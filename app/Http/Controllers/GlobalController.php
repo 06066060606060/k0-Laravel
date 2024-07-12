19504f295510184cab1e74b98e03215761e5d68f
Gokdocom <gokdo.com@gmail.com>
2024-07-12 14:06:01 +0200

    ok

--- app/Http/Controllers/GlobalController.php
+++ app/Http/Controllers/GlobalController.php
@@ -489,7 +489,7 @@ public function winner()
                 compact('leparrainne', 'leparrain', 'concours', 'idjoueur', 'scory', 'scores', 'orders', 'infos', 'paiements', 'joueursParraines')
             );
         } else {
-            return redirect('admin/login');
+            return redirect('admin/login'); 
         }
     }
     // achat en boutique lorsque le joueur confirme sa commande en boutique
