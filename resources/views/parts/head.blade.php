56cea543d2bf147bc7b0572b783053abf3697a14
Gokdocom <gokdo.com@gmail.com>
2024-07-24 14:54:21 +0200

    ok

--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -1,142 +1,128 @@
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-- Primary Meta Tags -->
-<meta name="title" content="Gokdo - Sondages Rémunérés">
-<title id="pageTitle">Sondages Rémunérés Gagnez de l'Argent en Ligne : Votre Avis Compte !</title>
-<meta id="pageDesc" name="description" content="{{__("Gagnez de l'argent en partageant votre opinion avec des sondages rémunérés en ligne. Inscrivez-vous dès maintenant pour des récompenses en échange de vos avis")}}">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Primary Meta Tags -->
+    <meta name="title" content="Gokdo - Sondages Rémunérés">
+    <title id="pageTitle">Sondages Rémunérés Gagnez de l'Argent en Ligne : Votre Avis Compte !</title>
+    <meta id="pageDesc" name="description" content="Gagnez de l'argent en partageant votre opinion avec des sondages rémunérés en ligne. Inscrivez-vous dès maintenant pour des récompenses en échange de vos avis">
+    <meta name="keywords" content="sondages rémunérés, enquêtes rémunérées, gagner de l'argent en ligne, panels de consommateurs, récompenses pour sondages, opinions rémunérées, participer à des sondages, sites de sondages, sondages payants, argent sur internet, sondages en ligne rémunérés, rémunération pour avis, panel d'opinion, compensations pour enquêtes, gains en répondant à des sondages, opportunités de revenus en ligne, rémunération pour feedback, études de marché payées, sondages rétribués, gagner de l'argent avec des sondages, travail en ligne rémunéré, plateformes de sondages, inscription à des panels de consommateurs, rémunération pour avis, gagner de l'argent avec des opinions, rémunération pour participation à des études, récompenses pour partager son opinion, rémunération pour réponses à des questionnaires en ligne">
+    <meta name="identifier-url" content="https://gokdo.com">
+    <meta http-equiv="content-language" content="fr">
+    <meta name="author" content="GoKDO">
+    <!-- Yandex -->
+    <meta name="yandex-verification" content="6e37c7dac9342d3a">
+    <meta name="robots" content="index,follow">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://gokdo.com/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://gokdo.com/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://gokdo.com/img/favicon-16x16.png">
+    <link rel="manifest" href="https://gokdo.com/img/site.webmanifest">
+    <link rel="mask-icon" href="https://gokdo.com/img/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://gokdo.com">
+    <meta property="og:title" content="Gokdo">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="https://gokdo.com/img/logo-sm.png">
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="#">
+    <meta property="twitter:title" content="Gokdo">
+    <meta property="twitter:description" content="">
+    <meta property="twitter:image" content="https://gokdo.com/img/logo-sm.png">
+    <meta name="coverage" content="Worldwide">
     
-<meta name="keywords" content="{{__("sondages rémunérés, enquêtes rémunérées, gagner de l'argent en ligne, panels de consommateurs, récompenses pour sondages, opinions rémunérées, participer à des sondages, sites de sondages, sondages payants, argent sur internet, sondages en ligne rémunérés, rémunération pour avis, panel d'opinion, compensations pour enquêtes, gains en répondant à des sondages, opportunités de revenus en ligne, rémunération pour feedback, études de marché payées, sondages rétribués, gagner de l'argent avec des sondages, travail en ligne rémunéré, plateformes de sondages, inscription à des panels de consommateurs, rémunération pour avis, gagner de l'argent avec des opinions, rémunération pour participation à des études, récompenses pour partager son opinion, rémunération pour réponses à des questionnaires en ligne")}}" />
-<meta name="identifier-url" content="https://gokdo.com" />
-<meta http-equiv="content-language" content="fr" />
-<meta name="author" content="GoKDO" />
-<!--Yandex-->
-<meta name="yandex-verification" content="6e37c7dac9342d3a" />
+    @php $url_en_cours = "https://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; @endphp
+    <link rel="canonical" href="{{ $url_en_cours }}">
 
-<meta name="robots" content="index,follow" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
+    
+    <!-- ADSENSE -->
+    <!--<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4337256114287476"
+        crossorigin="anonymous"></script>-->
+    
+    <script src="https://kit.fontawesome.com/59ecaaffaa.js" crossorigin="anonymous" async></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" async></script>
+    
+    <meta name="google-adsense-account" content="ca-pub-4337256114287476">
+    <meta name="site-verification" content="025856174dae3df55d27158e65fc17c6">
+    
+    <!-- Google tag (gtag.js) --> 
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11485783310"></script> 
+    <script> 
+        window.dataLayer = window.dataLayer || []; 
+        function gtag(){dataLayer.push(arguments);} 
+        gtag('js', new Date()); 
+        gtag('config', 'AW-11485783310'); 
+    </script>
+    
+    @if (backpack_auth()->check())
+    <!-- Analytics -->
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VNPT87NKHJ"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-VNPT87NKHJ');
+    </script>
+    @endif
 
-<link rel="apple-touch-icon" sizes="180x180" href="https://gokdo.com/img/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="https://gokdo.com/img/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="https://gokdo.com/img/favicon-16x16.png">
-<link rel="manifest" href="https://gokdo.com/img/site.webmanifest">
-<link rel="mask-icon" href="https://gokdo.com/img/safari-pinned-tab.svg" color="#5bbad5">
-<meta name="msapplication-TileColor" content="#da532c">
-<meta name="theme-color" content="#ffffff">
+    @vite('resources/css/app.css')
+    
+    <!-- LE TOKEN -->
+    <meta name="csrf-token" id="csrf-token" content="{{ csrf_token() }}">
+    <script>
+        myToken = <?php echo json_encode(['csrfToken' => csrf_token()]); ?>
+    </script>
 
-<!-- Open Graph / Facebook -->
-<meta property="og:type" content="website">
-<meta property="og:url" content="https://gokdo.com">
-<meta property="og:title" content="Gokdo">
-<meta property="og:description" content="">
-<meta property="og:image" content="https://gokdo.com/img/logo-sm.png">
-<!-- Twitter -->
-<meta property="twitter:card" content="summary_large_image">
-<meta property="twitter:url" content="#">
-<meta property="twitter:title" content="Gokdo">
-<meta property="twitter:description" content="">
-<meta property="twitter:image" content="https://gokdo.com/img/logo-sm.png">
+    @if(request()->path() == 'game')
+    <script>
+        if (window.location.href.match(/^https?:\/\/(www\.)?gokdo\.com(\/|$)/)) {
+            document.addEventListener("visibilitychange", function() {
+                // Si la page Web est visible
+                if (!document.hidden) {
+                    // Actualiser la page
+                    location.reload();
+                }
+                if (document.hidden) {
+                    // Actualiser la page
+                    location.reload();
+                }
+            });
+        }
 
-<meta name="coverage" content="Worldwide" />
+        // FONCTION DE DETECTION SI PLUSIEURS FENETRES SONT OUVERTES 
+        function detectMultipleWindows() {
+            // Vérifiez si le localStorage est disponible dans le navigateur
+            if (typeof(Storage) !== "undefined") {
+                // Récupérez le nombre de fenêtres ouvertes avec ce site
+                var numWindows = localStorage.getItem("numWindows") || 0;
+                // Incrémentez le nombre de fenêtres ouvertes
+                localStorage.setItem("numWindows", ++numWindows);
 
-@php $url_en_cours = "https://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]"; @endphp
+                // Lorsque la fenêtre est fermée, décrémentez le nombre de fenêtres ouvertes
+                window.addEventListener("beforeunload", function() {
+                    localStorage.setItem("numWindows", --numWindows);
+                });
+            } else {
+                console.log("localStorage n'est pas supporté par votre navigateur.");
+            }
+        }
 
-<link rel="canonical" href="{{ $url_en_cours }}" />
+        // APPELLE LA DETECTION DES FENETRES MULTIPLES
+        detectMultipleWindows();
+    </script>
+    @endif
+    <link rel="stylesheet" href="https://gokdo.com/appcss.min.css">
 
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css">
-
-<!-- ADSENSE -->
-<!--<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4337256114287476"
-     crossorigin="anonymous"></script>
--->
-
-<script src="https://kit.fontawesome.com/59ecaaffaa.js" crossorigin="anonymous" async></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" async></script>
-
-<meta name="google-adsense-account" content="ca-pub-4337256114287476">
-
-<meta name="site-verification" content="025856174dae3df55d27158e65fc17c6" />
-
-<!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11485783310"></script> <script> window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-11485783310'); </script>
-
-@if (backpack_auth()->check())
-<!--Analytics-->
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-VNPT87NKHJ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-VNPT87NKHJ');
-</script>
-@else 
-@endif
-
-@vite('resources/css/app.css')
-
-<!--LE TOKEN-->
-<meta name="csrf-token" id="csrf-token" content="{{ csrf_token() }}">
-<script>
-    myToken = <?php echo json_encode(['csrfToken' => csrf_token()]); ?>
-</script>
-
-@php
-// SI ON EST SUR LA PAGE DE JEU
-if(request()->path()=='game')  {
-@endphp
-<script>
-if (window.location.href.match(/^https?:\/\/(www\.)?gokdo\.com(\/|$)/)) {
-  document.addEventListener("visibilitychange", function() {
-    // Si la page Web est visible
-    if (!document.hidden) {
-      // Actualiser la page
-      location.reload();
+    @php
+    // FORCE LE HTTPS
+    // Vérifie si la connexion utilise le port HTTPS (port 443)
+    if ($_SERVER["SERVER_PORT"] != 443) {
+        // Redirige vers la version HTTPS de la même page
+        header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+        exit();
     }
-    if (document.hidden) {
-      // Actualiser la page
-      location.reload();
-    }
-  });
-}
-</script>
-
-// FONCTION DE DETECTION SI PLUSIEURS FENETRES SONT OUVERTES 
-<script>
-function detectMultipleWindows() {
-  // Vérifiez si le localStorage est disponible dans le navigateur
-  if (typeof(Storage) !== "undefined") {
-    // Récupérez le nombre de fenêtres ouvertes avec ce site
-    var numWindows = localStorage.getItem("numWindows") || 0;
-    // Incrémentez le nombre de fenêtres ouvertes
-    localStorage.setItem("numWindows", ++numWindows);
-
-    // Lorsque la fenêtre est fermée, décrémentez le nombre de fenêtres ouvertes
-    window.addEventListener("beforeunload", function() {
-      localStorage.setItem("numWindows", --numWindows);
-    });
-  } else {
-    console.log("localStorage n'est pas supporté par votre navigateur.");
-  }
-}
-
-//APPELLE LA DETECTION DES FENETRES MULTIPLES
-detectMultipleWindows();
-</script>
-@php
-} else { }
-@endphp
-<link rel="stylesheet" href="https://gokdo.com/appcss.min.css">
-
-
-@php
-// FORCE LE HTTPS
-// Vérifie si la connexion utilise le port HTTPS (port 443)
-if ($_SERVER["SERVER_PORT"] == 443) {
-    // La connexion est sécurisée, ne faites rien
-} else {
-    // Redirige vers la version HTTPS de la même page
-    header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-    exit();
-}
-// Le reste du code de votre page web
-@endphp
+    @endphp
